3a8e7405a6d5d7597b7d351955f33a5b1989ab8f
Gunnar Beutner <gunnar@beutner.name>
2012-05-29 06:25:01 +0200

    Updated Dictionary tests.

--- test/base-dictionary.cpp
+++ test/base-dictionary.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_CASE(construct)
 	BOOST_REQUIRE(dictionary);
 }
 
-BOOST_AUTO_TEST_CASE(setproperty)
+BOOST_AUTO_TEST_CASE(getproperty)
 {
 	Dictionary::Ptr dictionary = make_shared<Dictionary>();
 	dictionary->SetProperty("test1", 7);
@@ -24,4 +24,32 @@ BOOST_AUTO_TEST_CASE(setproperty)
 	string test2;
 	BOOST_REQUIRE(dictionary->GetProperty("test2", &test2));
 	BOOST_REQUIRE(test2 == "hello world");
+
+	long test3;
+	BOOST_REQUIRE(!dictionary->GetProperty("test3", &test3));
+}
+
+BOOST_AUTO_TEST_CASE(getproperty_dict)
+{
+	Dictionary::Ptr dictionary = make_shared<Dictionary>();
+	Dictionary::Ptr other = make_shared<Dictionary>();
+
+	dictionary->SetProperty("test1", other);
+
+	Dictionary::Ptr test1;
+	BOOST_REQUIRE(dictionary->GetProperty("test1", &test1));
+	BOOST_REQUIRE(other == test1);
+
+	Dictionary::Ptr test2;
+	BOOST_REQUIRE(!dictionary->GetProperty("test2", &test2));
+}
+
+BOOST_AUTO_TEST_CASE(unnamed)
+{
+	Dictionary::Ptr dictionary = make_shared<Dictionary>();
+	dictionary->AddUnnamedProperty("test1");
+	dictionary->AddUnnamedProperty("test2");
+	dictionary->AddUnnamedProperty("test3");
+
+	BOOST_REQUIRE(distance(dictionary->Begin(), dictionary->End()) == 3);
 }
