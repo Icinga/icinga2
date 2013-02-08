01cde2195de25b9afc2acaf39f16f14a9b28731f
Gunnar Beutner <gunnar@beutner.name>
2013-02-08 06:49:23 +0100

    Build unit tests on Windows.
    Refs #3645

--- test/base-dictionary.cpp
+++ test/base-dictionary.cpp
@@ -1,9 +1,29 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE base_dictionary
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012 Icinga Development Team (http://www.icinga.org/)        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
 #include <boost/test/unit_test.hpp>
 
 #include <i2-base.h>
+
 using namespace icinga;
+
+BOOST_AUTO_TEST_SUITE(base_dictionary)
 
 BOOST_AUTO_TEST_CASE(construct)
 {
@@ -70,3 +90,5 @@ BOOST_AUTO_TEST_CASE(unnamed_order)
 		i++;
 	}
 }
+
+BOOST_AUTO_TEST_SUITE_END()
