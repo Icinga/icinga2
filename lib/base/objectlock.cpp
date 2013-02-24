022be22fa2d548fb4817f6e7b3f89dad22e605b1
Gunnar Beutner <gunnar.beutner@netways.de>
2013-02-24 08:27:00 +0100

    Added some missing files.

---
+++ lib/base/objectlock.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012 Icinga Development Team (http://www.icinga.org/)        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
+#include "i2-base.h"
+
+using namespace icinga;
+
+ObjectLock::ObjectLock(void)
+	: m_Object(NULL), m_Lock()
+{ }
+
+ObjectLock::ObjectLock(const Object::Ptr& object)
+	: m_Object(object.get()), m_Lock()
+{
+	if (m_Object)
+		Lock();
+}
+
+ObjectLock::ObjectLock(const Object *object)
+	: m_Object(object), m_Lock()
+{
+	if (m_Object)
+		Lock();
+}
+
+void ObjectLock::Lock(void)
+{
+	assert(!m_Lock.owns_lock() && m_Object != NULL);
+
+	m_Lock = recursive_mutex::scoped_lock(m_Object->GetMutex());
+}
+
+void ObjectLock::Unlock(void)
+{
+	m_Lock = recursive_mutex::scoped_lock();
+}
