09d97c551e2a4c5370f5d47ca32b2c02e4017ff2
Gunnar Beutner <gunnar.beutner@netways.de>
2013-04-19 13:51:33 +0200

    Fix 'make check-coverage' for VPATH builds.

---
+++ test/base-match.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012 Icinga Development Team (http://www.icinga.org/)        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
+#include "base/utility.h"
+#include <boost/test/unit_test.hpp>
+
+using namespace icinga;
+
+BOOST_AUTO_TEST_SUITE(base_match)
+
+BOOST_AUTO_TEST_CASE(tolong)
+{
+	BOOST_CHECK(Utility::Match("*", "hello"));
+	BOOST_CHECK(!Utility::Match("world", "hello"));
+	BOOST_CHECK(!Utility::Match("hee*", "hello"));
+	BOOST_CHECK(Utility::Match("he??o", "hello"));
+	BOOST_CHECK(Utility::Match("he?", "hel"));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
