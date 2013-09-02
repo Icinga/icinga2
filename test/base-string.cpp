2e81c284444f9da3528670f163c7ae4a9bf36769
Gunnar Beutner <gunnar.beutner@netways.de>
2013-09-02 13:08:29 +0200

    Add more unit tests.

---
+++ test/base-string.cpp
@@ -0,0 +1,111 @@
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012 Icinga Development Team (http://www.icinga.org/)        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
+#include "base/qstring.h"
+#include <boost/test/unit_test.hpp>
+#include <boost/foreach.hpp>
+
+using namespace icinga;
+
+BOOST_AUTO_TEST_SUITE(base_string)
+
+BOOST_AUTO_TEST_CASE(construct)
+{
+	BOOST_CHECK(String() == "");
+	BOOST_CHECK(String(5, 'n') == "nnnnn");
+}
+
+BOOST_AUTO_TEST_CASE(equal)
+{
+	BOOST_CHECK(String("hello") == String("hello"));
+	BOOST_CHECK("hello" == String("hello"));
+	BOOST_CHECK(String("hello") == String("hello"));
+
+	BOOST_CHECK(String("hello") != String("helloworld"));
+	BOOST_CHECK("hello" != String("helloworld"));
+	BOOST_CHECK(String("hello") != "helloworld");
+}
+
+BOOST_AUTO_TEST_CASE(clear)
+{
+	String s = "hello";
+	s.Clear();
+	BOOST_CHECK(s == "");
+	BOOST_CHECK(s.IsEmpty());
+}
+
+BOOST_AUTO_TEST_CASE(append)
+{
+	String s;
+	s += "he";
+	s += String("ll");
+	s += 'o';
+
+	BOOST_CHECK(s == "hello");
+}
+
+BOOST_AUTO_TEST_CASE(trim)
+{
+	String s1 = "hello";
+	s1.Trim();
+	BOOST_CHECK(s1 == "hello");
+
+	String s2 = "  hello";
+	s2.Trim();
+	BOOST_CHECK(s2 == "hello");
+
+	String s3 = "hello ";
+	s3.Trim();
+	BOOST_CHECK(s3 == "hello");
+
+	String s4 = " hello ";
+	s4.Trim();
+	BOOST_CHECK(s4 == "hello");
+}
+
+BOOST_AUTO_TEST_CASE(replace)
+{
+	String s = "hello";
+
+	s.Replace(0, 2, "x");
+	BOOST_CHECK(s == "xllo");
+}
+
+BOOST_AUTO_TEST_CASE(index)
+{
+	String s = "hello";
+	BOOST_CHECK(s[0] == 'h');
+
+	s[0] = 'x';
+	BOOST_CHECK(s == "xello");
+
+	BOOST_FOREACH(char& ch, s) {
+		ch = 'y';
+	}
+	BOOST_CHECK(s == "yyyyy");
+}
+
+BOOST_AUTO_TEST_CASE(find)
+{
+	String s = "hello";
+	BOOST_CHECK(s.Find("ll") == 2);
+	BOOST_CHECK(s.FindFirstOf("xl") == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
