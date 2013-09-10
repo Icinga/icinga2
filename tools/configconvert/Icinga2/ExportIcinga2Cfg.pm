dc0f2e2fa976b35ab13fa66dacec9fe61a52d0b7
Michael Friedrich <michael.friedrich@netways.de>
2013-09-10 14:04:18 +0200

    Config Conversion: Skip ARGn macros on Command object export.

--- tools/configconvert/Icinga2/ExportIcinga2Cfg.pm
+++ tools/configconvert/Icinga2/ExportIcinga2Cfg.pm
@@ -1094,15 +1094,13 @@ sub dump_command_2x {
     if(defined($command_2x->{'__I2CONVERT_COMMAND_MACROS'}) && $command_2x->{'__I2CONVERT_COMMAND_MACROS'} != 0) {
         dump_config_line($icinga2_cfg, "\tmacros = {");
         foreach my $cmd_arg (keys %{$command_2x->{'__I2CONVERT_COMMAND_MACROS'}}) {
+            next if $cmd_arg =~ /ARG\d+/;
             dump_config_line($icinga2_cfg, "\t\t$cmd_arg = \"$command_2x->{'__I2CONVERT_COMMAND_MACROS'}->{$cmd_arg}\",");
         }
         dump_config_line($icinga2_cfg, "\t},");
     }
 
     dump_config_line($icinga2_cfg, "");
-
-    dump_config_line($icinga2_cfg, "");
-
     dump_config_line($icinga2_cfg, "}");
     dump_config_line($icinga2_cfg, "\n");
 }
