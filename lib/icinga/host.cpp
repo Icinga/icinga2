61f0f7edc7965123ef12471cd2e395d62c333b4a
Gunnar Beutner <gunnar.beutner@netways.de>
2013-09-10 10:54:23 +0200

    Don't copy macros / custom attributes to services.

--- lib/icinga/host.cpp
+++ lib/icinga/host.cpp
@@ -231,9 +231,6 @@ void Host::UpdateSlaveServices(void)
 		keys.insert("check_period");
 		keys.insert("servicedependencies");
 		keys.insert("hostdependencies");
-		keys.insert("export_macros");
-		keys.insert("macros");
-		keys.insert("custom");
 
 		ExpressionList::Ptr host_exprl = boost::make_shared<ExpressionList>();
 		item->GetLinkedExpressionList()->ExtractFiltered(keys, host_exprl);
