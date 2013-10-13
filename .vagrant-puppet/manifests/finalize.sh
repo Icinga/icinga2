11b71774b42ef1f9a0a5cabb30b99b8a2137fcbd
Michael Friedrich <Michael.Friedrich@netways.de>
2013-10-13 21:52:47 +0200

    Vagrant: Fix permission issues, start icinga2.

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# for some reason puppet does not start icinga2
+/sbin/service icinga2 start
+
+# fix faulty file permissions
+chown root:apache /etc/icinga/passwd
+/sbin/service httpd reload
+
 echo "The Icinga 2 Vagrant VM has finished installing. See http://localhost:8080/ for more details."
