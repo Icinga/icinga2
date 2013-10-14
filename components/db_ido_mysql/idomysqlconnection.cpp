def9b309a5de3444a9d2475b31fd36d4159832cc
Gunnar Beutner <gunnar.beutner@netways.de>
2013-10-14 14:11:27 +0200

    Add missing quotes.

--- components/db_ido_mysql/idomysqlconnection.cpp
+++ components/db_ido_mysql/idomysqlconnection.cpp
@@ -157,8 +157,8 @@ void IdoMysqlConnection::ReconnectTimerHandler(void)
 		/* record connection */
 		Query("INSERT INTO " + GetTablePrefix() + "conninfo " +
 		    "(instance_id, connect_time, last_checkin_time, agent_name, agent_version, connect_type, data_start_time) VALUES ("
-		    + Convert::ToString(static_cast<long>(m_InstanceID)) + ", NOW(), NOW(), 'icinga2 db_ido_mysql', " + Escape(Application::GetVersion())
-		    + ", '" + (reconnect ? "RECONNECT" : "INITIAL") + "', NOW())");
+		    + Convert::ToString(static_cast<long>(m_InstanceID)) + ", NOW(), NOW(), 'icinga2 db_ido_mysql', '" + Escape(Application::GetVersion())
+		    + "', '" + (reconnect ? "RECONNECT" : "INITIAL") + "', NOW())");
 
 		/* clear config tables for the initial config dump */
 		ClearConfigTables();
