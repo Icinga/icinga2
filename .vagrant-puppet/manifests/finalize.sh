607d4451d66db022b362987dc59687b500c9ac63
Gunnar Beutner <gunnar.beutner@netways.de>
2014-04-08 11:13:36 +0200

    Update Vagrant documentation URL.
    Refs #5774

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "The Icinga 2 Vagrant VM has finished installing. See http://localhost:8080/ for more details."
+echo "The Icinga 2 Vagrant VM has finished installing. See http://docs.icinga.org/icinga2/snapshot/chapter-9.html for more details."
