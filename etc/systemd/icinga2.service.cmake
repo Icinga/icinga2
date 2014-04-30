69c06115d2dd27f665bfe9955e9648580f52ef8b
Gerd von Egidy <gerd@egidy.de>
2014-05-01 01:48:01 +0200

    First try at a systemd service definition file, install it from CMake.
    Native systemd support is enabled with cmake -DUSE_SYSTEMD=ON
    
    Refs #4794

---
+++ etc/systemd/icinga2.service.cmake
@@ -0,0 +1,16 @@
+[Unit]
+Description=Icinga host/service/network monitoring system
+After=syslog.target postgresql.service mariadb.service
+
+[Service]
+Type=forking
+EnvironmentFile=/etc/sysconfig/icinga2
+# ExecStartPre=  TODO: execute the mkdir & chown/chmod stuff, ideally in a separate script, used by both init.d and systemd
+ExecStart=@CMAKE_INSTALL_FULL_SBINDIR@/icinga2 -c ${ICINGA2_CONFIG_FILE} -d -e ${ICINGA2_ERROR_LOG} -u ${ICINGA2_USER} -g ${ICINGA2_GROUP}
+PIDFile=@CMAKE_INSTALL_FULL_LOCALSTATEDIR@/run/icinga2/icinga2.pid
+
+# TODO: reload not working, seems like systemd kills the newly forked daemon process
+#ExecReload=/bin/kill -HUP $MAINPID
+
+[Install]
+WantedBy=multi-user.target
