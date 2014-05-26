7183495d8959621d6eb1209959d47e77e3ffd1a5
Gunnar Beutner <gunnar.beutner@netways.de>
2014-05-26 09:01:23 +0200

    Make username in the logrotate config configurable.
    Fixes #6295

--- etc/logrotate.d/icinga2.cmake
+++ etc/logrotate.d/icinga2.cmake
@@ -1,11 +1,11 @@
-/var/log/icinga2/icinga2.log /var/log/icinga2/debug.log {
+@CMAKE_INSTALL_FULL_LOCALSTATEDIR@/log/icinga2/icinga2.log @CMAKE_INSTALL_FULL_LOCALSTATEDIR@/log/icinga2/debug.log {
        	daily
 	rotate 7
 	compress
 	delaycompress
        	missingok
        	notifempty
-       	create 644 icinga icinga
+       	create 644 @ICINGA2_USER@ @ICINGA2_GROUP@
 	copytruncate
 	postrotate
 		if ! killall -q -USR1 icinga2; then
@@ -14,14 +14,14 @@
        	endscript
 }
 
-/var/log/icinga2/error.log {
+@CMAKE_INSTALL_FULL_LOCALSTATEDIR@/log/icinga2/error.log {
 	daily
 	rotate 90
 	compress
 	delaycompress
 	missingok
 	notifempty
-	create 644 icinga icinga
+	create 644 @ICINGA2_USER@ @ICINGA2_GROUP@
 	copytruncate
 	# TODO: figure out how to get Icinga to re-open this log file
 }
