1a361d834ade415957a6b7f7f38e206759465009
Gunnar Beutner <gunnar.beutner@netways.de>
2014-07-23 10:25:26 +0200

    Add MySQL/pgSQL upgrade files which fix name2 in the icinga_objects table
    fixes #6646

---
+++ components/db_ido_pgsql/schema/upgrade/pgsql-upgrade-2.0.2.sql
@@ -0,0 +1,10 @@
+-- -----------------------------------------
+-- upgrade path for Icinga 2.0.2
+--
+-- -----------------------------------------
+-- Copyright (c) 2014 Icinga Development Team (http://www.icinga.org)
+--
+-- Please check http://docs.icinga.org for upgrading information!
+-- -----------------------------------------
+
+UPDATE icinga_objects SET name2 = NULL WHERE name2 = '';
