ac47649722e61299af13bcec6f4a9334eb5c7cdb
Gunnar Beutner <gunnar@beutner.name>
2014-10-17 14:37:43 +0200

    Remove unnecessary arguments from the systemd service file

--- etc/initsystem/icinga2.service.cmake
+++ etc/initsystem/icinga2.service.cmake
@@ -6,7 +6,7 @@ After=syslog.target postgresql.service mariadb.service carbon-cache.service
 Type=forking
 EnvironmentFile=@ICINGA2_SYSCONFIGFILE@
 ExecStartPre=@CMAKE_INSTALL_FULL_SBINDIR@/icinga2-prepare-dirs @ICINGA2_SYSCONFIGFILE@
-ExecStart=@CMAKE_INSTALL_FULL_SBINDIR@/icinga2 daemon -c ${ICINGA2_CONFIG_FILE} -d -e ${ICINGA2_ERROR_LOG}
+ExecStart=@CMAKE_INSTALL_FULL_SBINDIR@/icinga2 daemon -d -e ${ICINGA2_ERROR_LOG}
 PIDFile=@ICINGA2_RUNDIR@/icinga2/icinga2.pid
 ExecReload=/bin/kill -HUP $MAINPID
 
