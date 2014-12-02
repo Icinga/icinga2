ac6768d870d45637868be2541eaba9ce5cc1ad93
Tim Hardeck <thardeck@suse.de>
2014-12-02 18:26:29 +0100

    Use Icinga 1.x Apache conf
    This Apache conf is from Icinga 1.x and should work with Apache 2.2 and
    2.4.
    
    fixes #7929

--- etc/icinga/icinga-classic-apache.conf.cmake
+++ etc/icinga/icinga-classic-apache.conf.cmake
@@ -11,20 +11,28 @@ ScriptAlias /icinga/cgi-bin "@CMAKE_INSTALL_FULL_LIBDIR@/icinga/cgi"
 #  SSLRequireSSL
    Options ExecCGI
    AllowOverride None
-   Order allow,deny
-   <IfVersion < 2.3>
-      Allow from all
-#     Order deny,allow
-#     Deny from all
-#     Allow from 127.0.0.1
-   </IfVersion>
-   <IfVersion >= 2.3>
-      Require all granted
-   </IfVersion>
+
    AuthName "Icinga Access"
    AuthType Basic
    AuthUserFile @CMAKE_INSTALL_FULL_SYSCONFDIR@/icinga/passwd
-   Require valid-user
+
+   <IfModule mod_authz_core.c>
+      # Apache 2.4
+      <RequireAll>
+         Require all granted
+         # Require local
+         Require valid-user
+      </RequireAll>
+   </IfModule>
+   <IfModule !mod_authz_core.c>
+      # Apache 2.2
+      Order allow,deny
+      Allow from all
+      #  Order deny,allow
+      #  Deny from all
+      #  Allow from 127.0.0.1
+      Require valid-user
+    </IfModule>
 </Directory>
 
 Alias /icinga "@CMAKE_INSTALL_FULL_DATADIR@/icinga/"
@@ -33,19 +41,27 @@ Alias /icinga "@CMAKE_INSTALL_FULL_DATADIR@/icinga/"
 #  SSLRequireSSL
    Options None
    AllowOverride All
-   <IfVersion < 2.3>
-      Order allow,deny
-      Allow from all
-#     Order deny,allow
-#     Deny from all
-#     Allow from 127.0.0.1
-   </IfVersion>
-   <IfVersion >= 2.3>
-      Require all granted
-   </IfVersion>
+
    AuthName "Icinga Access"
    AuthType Basic
    AuthUserFile @CMAKE_INSTALL_FULL_SYSCONFDIR@/icinga/passwd
-   Require valid-user
+
+   <IfModule mod_authz_core.c>
+      # Apache 2.4
+      <RequireAll>
+         Require all granted
+         # Require local
+         Require valid-user
+      </RequireAll>
+   </IfModule>
+   <IfModule !mod_authz_core.c>
+      # Apache 2.2
+      Order allow,deny
+      Allow from all
+      #  Order deny,allow
+      #  Deny from all
+      #  Allow from 127.0.0.1
+      Require valid-user
+   </IfModule>
 </Directory>
 
