78bfd0204c376d87129b3140ceef027f63dcf63e
Michael Friedrich <michael.friedrich@netways.de>
2015-01-22 12:00:23 +0100

    Update copyright year

--- icinga-app/icinga.cpp
+++ icinga-app/icinga.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Icinga 2                                                                   *
- * Copyright (C) 2012-2014 Icinga Development Team (http://www.icinga.org)    *
+ * Copyright (C) 2012-2015 Icinga Development Team (http://www.icinga.org)    *
  *                                                                            *
  * This program is free software; you can redistribute it and/or              *
  * modify it under the terms of the GNU General Public License                *
@@ -318,7 +318,7 @@ int Main(void)
 			}
 
 			if (vm.count("version")) {
-				std::cout << "Copyright (c) 2012-2014 Icinga Development Team (https://www.icinga.org)" << std::endl
+				std::cout << "Copyright (c) 2012-2015 Icinga Development Team (https://www.icinga.org)" << std::endl
 					<< "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl2.html>" << std::endl
 					<< "This is free software: you are free to change and redistribute it." << std::endl
 					<< "There is NO WARRANTY, to the extent permitted by law.";
--- plugins/README.md
+++ plugins/README.md
@@ -14,7 +14,7 @@ Call a plugin with the "--help" option to receive information about its usage.
 ###License###
 
  Icinga 2                                                                   
- Copyright (C) 2012-2014 Icinga Development Team (http://www.icinga.org) 
+ Copyright (C) 2012-2015 Icinga Development Team (http://www.icinga.org) 
 
  This program is free software; you can redistribute it and/or modify it under the tems of the GNU General Public License as published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.                                                                                                
