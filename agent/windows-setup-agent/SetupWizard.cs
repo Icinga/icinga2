b83e207eb7dec49bb3fa944cc1a21707dd7afe61
Gunnar Beutner <gunnar@beutner.name>
2015-02-10 09:31:41 +0100

    Add another missing directory
    refs #7995

--- agent/windows-setup-agent/SetupWizard.cs
+++ agent/windows-setup-agent/SetupWizard.cs
@@ -294,6 +294,7 @@ namespace Icinga
 			Directory.CreateDirectory(installDir + "\\var\\lib\\icinga2\\api\\log");
 			Directory.CreateDirectory(installDir + "\\var\\lib\\icinga2\\api\\zones");
 			Directory.CreateDirectory(installDir + "\\var\\log\\icinga2\\compat\\archive");
+			Directory.CreateDirectory(installDir + "\\var\\log\\icinga2\\crash");
 			Directory.CreateDirectory(installDir + "\\var\\run\\icinga2\\cmd");
 			Directory.CreateDirectory(installDir + "\\var\\spool\\icinga2\\perfdata");
 			Directory.CreateDirectory(installDir + "\\var\\spool\\icinga2\\tmp");
