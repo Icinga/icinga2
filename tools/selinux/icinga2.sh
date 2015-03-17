167f43a2314d8eb7d5044e5d3ba2efcfec8fa56f
Dirk Goetz <dirk.goetz@netways.de>
2015-03-17 21:23:02 +0000

    added chcon to systemd safe-reload script Selinux: Added support for notifications
    refs #8332

--- tools/selinux/icinga2.sh
+++ tools/selinux/icinga2.sh
@@ -48,6 +48,8 @@ sepolicy manpage -p . -d icinga2_t
 /sbin/restorecon -F -R -v /usr/sbin/icinga2
 # Fixing the file context on /etc/rc\.d/init\.d/icinga2
 #/sbin/restorecon -F -R -v /etc/rc\.d/init\.d/icinga2
+# Fixing the file context on /etc/icinga2/scripts
+/sbin/restorecon -F -R -v /etc/icinga2/scripts
 # Fixing the file context on /var/log/icinga2
 /sbin/restorecon -F -R -v /var/log/icinga2
 # Fixing the file context on /var/lib/icinga2
@@ -59,10 +61,13 @@ sepolicy manpage -p . -d icinga2_t
 # Fixing the file context on /var/spool/icinga2
 /sbin/restorecon -F -R -v /var/spool/icinga2
 
+# Fix dir permissions until we have it in the package
+chown root /etc/icinga2
+chown root /etc/icinga2/init.conf
+
 # Label the port 5665
 /sbin/semanage port -a -t icinga2_port_t -p tcp 5665
 
 # Generate a rpm package for the newly generated policy
-
 pwd=$(pwd)
 #rpmbuild --define "_sourcedir ${pwd}" --define "_specdir ${pwd}" --define "_builddir ${pwd}" --define "_srcrpmdir ${pwd}" --define "_rpmdir ${pwd}" --define "_buildrootdir ${pwd}/.build"  -ba icinga2_selinux.spec
