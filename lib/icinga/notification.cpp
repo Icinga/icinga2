4b2e1c2662cb3ede51f7eb9bf4bafa5b346a02d9
Michael Friedrich <michael.friedrich@netways.de>
2015-04-12 15:11:38 +0200

    Change log level to 'notice' for 'Attempting to send notifications' messages
    fixes #9021

--- lib/icinga/notification.cpp
+++ lib/icinga/notification.cpp
@@ -233,7 +233,7 @@ void Notification::BeginExecuteNotification(NotificationType type, const CheckRe
 {
 	ASSERT(!OwnsLock());
 
-	Log(LogInformation, "Notification")
+	Log(LogNotice, "Notification")
 	    << "Attempting to send notifications for notification object '" << GetName() << "'.";
 
 	Checkable::Ptr checkable = GetCheckable();
