c9eccaac4008ec7432f40778d488e93be03c102e
Gunnar Beutner <gunnar@beutner.name>
2015-05-12 13:49:52 +0200

    Improve user experience when updating Icinga 2 with the NSIS installer
    fixes #8116

--- agent/windows-setup-agent/Program.cs
+++ agent/windows-setup-agent/Program.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace Icinga
 {
 	static class Program
 	{
+
+		public static string Icinga2InstallDir
+		{
+			get
+			{
+				RegistryKey rk = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Icinga Development Team\\ICINGA2");
+
+				if (rk == null)
+					return "";
+
+				return (string)rk.GetValue("");
+			}
+		}
+
+		public static void FatalError(Form owner, string message)
+		{
+			MessageBox.Show(owner, message, owner.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Application.Exit();
+		}
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
@@ -13,7 +35,20 @@ namespace Icinga
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new SetupWizard());
+
+			string installDir = Program.Icinga2InstallDir;
+
+			if (installDir == "")
+				FatalError(null, "Icinga 2 does not seem to be installed properly.");
+
+			Form form;
+
+			if (File.Exists(installDir + "\\etc\\icinga2\\features-enabled\\api.conf"))
+				form = new ServiceStatus();
+			else
+				form = new SetupWizard();
+
+			Application.Run(form);
 		}
 	}
 }
