5f821f8560a44db934fd89091fe052a1c1865ce9
Gunnar Beutner <gunnar@beutner.name>
2015-05-19 09:04:10 +0200

    Add source code for i2eval to the contrib directory

---
+++ contrib/i2eval/i2tcl.hpp
@@ -0,0 +1,23 @@
+#ifdef SWIG
+%module i2tcl
+%{
+#include "i2tcl.hpp"
+%}
+
+%typemap(in,numinputs=0) Tcl_Interp *interp {  
+	$1 = interp;
+}
+
+#endif /* SWIG */
+
+#include <tcl.h>
+
+#ifndef I2TCL_H
+#define I2TCL_H
+
+void i2_register_command(const char *icmd, const char *tcmd, const char *mtype, Tcl_Interp *interp);
+void *i2_new_frame(Tcl_Interp *interp);
+void i2_free_frame(void *frame, Tcl_Interp *interp);
+char *i2_eval(void *frame, const char *text, Tcl_Interp *interp);
+
+#endif /* I2TCL_H */
