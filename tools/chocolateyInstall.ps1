57647ce1e740cc84f652eecd5347e50a4ef5e314
Gunnar Beutner <gunnar@beutner.name>
2015-06-18 11:42:33 +0200

    Add spec file for Chocolatey
    fixes #8503

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'icinga2'
+$installerType = 'exe'
+$url = 'http://packages.icinga.org/windows/Icinga2-v2.3.5.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
