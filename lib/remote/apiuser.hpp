14f5986aa52c8e6568907e14f90805f90075f531
Michael Friedrich <michael.friedrich@netways.de>
2015-06-22 17:51:11 +0200

    Implement the ApiUser object type
    Hide password in GetPassword() and add CheckPassword().
    Includes basic unit tests.
    
    refs #9471

---
+++ lib/remote/apiuser.hpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012-2015 Icinga Development Team (http://www.icinga.org)    *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
+#ifndef APIUSER_H
+#define APIUSER_H
+
+#include "remote/i2-remote.hpp"
+#include "remote/apiuser.thpp"
+
+namespace icinga
+{
+
+/**
+ * @ingroup remote
+ */
+class I2_REMOTE_API ApiUser : public ObjectImpl<ApiUser>
+{
+public:
+	DECLARE_OBJECT(ApiUser);
+	DECLARE_OBJECTNAME(ApiUser);
+
+	String GetPassword(void) const;
+	void SetPassword(const String& password);
+	bool CheckPassword(const String& password);
+};
+
+}
+
+#endif /* APIUSER_H */
