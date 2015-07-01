93177ee09ab71c9c5498bfc12fd3619fa5b43935
Gunnar Beutner <gunnar@beutner.name>
2015-07-01 08:48:42 +0200

    Fix build problems on AIX
    fixes #9531

--- third-party/socketpair/socketpair.c
+++ third-party/socketpair/socketpair.c
@@ -146,7 +146,7 @@ int dumb_socketpair(int socks[2], int dummy)
         errno = EINVAL;
         return -1;
     }
-    dummy = socketpair(AF_LOCAL, SOCK_STREAM, 0, socks);
+    dummy = socketpair(AF_UNIX, SOCK_STREAM, 0, socks);
     if (dummy)
         socks[0] = socks[1] = -1;
     return dummy;
