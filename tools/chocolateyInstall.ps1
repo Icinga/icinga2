bcdcfa9f81209a30b5f5e02f0960b9e3373f724d
Gunnar Beutner <gunnar@beutner.name>
2015-11-26 11:00:12 +0100

    Release version 2.4.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'icinga2'
 $installerType = 'exe'
-$url = 'http://packages.icinga.org/windows/Icinga2-v2.4.0.exe'
+$url = 'http://packages.icinga.org/windows/Icinga2-v2.4.1.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
