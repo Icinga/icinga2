a51e647cc760bd5f7c4de6182961a477478c11a9
Michael Friedrich <michael.friedrich@netways.de>
2016-01-20 16:22:23 +0100

    Fix check scheduling w/ retry_interval
    fixes #7287

--- lib/icinga/checkable-check.cpp
+++ lib/icinga/checkable-check.cpp
@@ -311,6 +311,9 @@ void Checkable::ProcessCheckResult(const CheckResult::Ptr& cr, const MessageOrig
 		UpdateFlappingStatus(stateChange);
 	is_flapping = IsFlapping();
 
+	/* update next check time on possible state changes */
+	UpdateNextCheck();
+
 	olock.Unlock();
 
 //	Log(LogDebug, "Checkable")
