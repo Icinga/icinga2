6cd931726da6b20b8fdf892ba6e58602ae3f74ac
Gunnar Beutner <gunnar@beutner.name>
2016-02-04 13:17:37 +0100

    Fix crash in ConfigItem::RunWithActivationContext
    fixes #11085

--- lib/config/configitem.cpp
+++ lib/config/configitem.cpp
@@ -521,8 +521,6 @@ bool ConfigItem::CommitItems(const ActivationContext::Ptr& context, WorkQueue& u
 		return false;
 	}
 
-	ASSERT(newItems.size() > 0);
-
 	ApplyRule::CheckMatches();
 
 	/* log stats for external parsers */
@@ -592,6 +590,9 @@ bool ConfigItem::ActivateItems(WorkQueue& upq, const std::vector<ConfigItem::Ptr
 bool ConfigItem::RunWithActivationContext(const Function::Ptr& function)
 {
 	ActivationScope scope;
+
+	if (!function)
+		BOOST_THROW_EXCEPTION(ScriptError("'function' argument must not be null."));
 
 	{
 		ScriptFrame frame;
