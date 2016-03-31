948c8f134252b2ef693b750fed3eeeb87eb02299
Gunnar Beutner <gunnar@beutner.name>
2016-03-31 13:54:10 +0200

    Make sure config files aren't overwritten during updates
    refs #11449

--- cmake/InstallConfig.cmake
+++ cmake/InstallConfig.cmake
@@ -21,25 +21,20 @@ function(install_if_not_exists src dest)
     set(src "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
   endif()
   get_filename_component(src_name "${src}" NAME)
-  if (NOT IS_ABSOLUTE "${dest}")
-    set(dest "${CMAKE_INSTALL_PREFIX}/${dest}")
-  endif()
   get_filename_component(basename_dest "${src}" NAME)
   string(REPLACE "/" "\\\\" nsis_src "${src}")
   string(REPLACE "/" "\\\\" nsis_dest_dir "${real_dest}")
   string(REPLACE "/" "\\\\" nsis_dest "${real_dest}/${basename_dest}")
-  set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}
-    SetOverwrite off
-    CreateDirectory '$INSTDIR\\\\${nsis_dest_dir}'
-    File '/oname=${nsis_dest}' '${nsis_src}'
-    SetOverwrite on
-  " PARENT_SCOPE)
   install(CODE "
-    if(NOT EXISTS \"\$ENV{DESTDIR}${dest}/${src_name}\")
-      #file(INSTALL \"${src}\" DESTINATION \"${dest}\")
+    if(\${CMAKE_INSTALL_PREFIX} MATCHES .*/_CPack_Packages/.* OR NOT EXISTS \"\$ENV{DESTDIR}${dest}/${src_name}\")
       message(STATUS \"Installing: \$ENV{DESTDIR}${dest}/${src_name}\")
+      if(\${CMAKE_INSTALL_PREFIX} MATCHES .*/_CPack_Packages/.*)
+        set(skel_prefix \"share/skel/\")
+      else()
+        set(skel_prefix \"\")
+      endif()
       execute_process(COMMAND \${CMAKE_COMMAND} -E copy \"${src}\"
-                      \"\$ENV{DESTDIR}${dest}/${src_name}\"
+                      \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/\${skel_prefix}${dest}/${src_name}\"
                       RESULT_VARIABLE copy_result
                       ERROR_VARIABLE error_output)
       if(copy_result)
