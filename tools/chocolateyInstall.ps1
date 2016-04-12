4fe7076b5f9818fc0936a1617ffd657fbcf8e24b
Gunnar Beutner <gunnar.beutner@netways.de>
2016-04-12 12:27:33 +0200

    Update Chocolatey scripts to support the new MSI package
    refs #11449

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = 'icinga2'
-$installerType = 'exe'
-$url = 'http://packages.icinga.org/windows/Icinga2-v2.4.4.exe'
-$silentArgs = '/S'
+$installerType = 'msi'
+$url32 = 'http://packages.icinga.org/windows/Icinga2-snapshot-x86.msi'
+$url64 = 'http://packages.icinga.org/windows/Icinga2-snapshot-x86_64.msi'
+$silentArgs = '/silent'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64" -validExitCodes $validExitCodes
