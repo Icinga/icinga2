b6435142f72b0f09c4750675c880e28a192e8f54
Michael Friedrich <michael.friedrich@netways.de>
2016-04-21 14:03:42 +0200

    Update chocolatey packages and RELEASE.md
    fixes #11647

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'msi'
 $url32 = 'http://packages.icinga.org/windows/Icinga2-v2.4.7-x86.msi'
 $url64 = 'http://packages.icinga.org/windows/Icinga2-v2.4.7-x86_64.msi'
-$silentArgs = '/silent'
+$silentArgs = '/qn /norestart'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64" -validExitCodes $validExitCodes
