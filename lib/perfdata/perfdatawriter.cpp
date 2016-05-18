bb69540b32c833720ed69318fb04572e64ffaee7
Gunnar Beutner <gunnar.beutner@netways.de>
2016-05-18 14:01:32 +0200

    Fix exception in PerfdataWriter::RotateFile
    fixes #11801

--- lib/perfdata/perfdatawriter.cpp
+++ lib/perfdata/perfdatawriter.cpp
@@ -123,12 +123,14 @@ void PerfdataWriter::RotateFile(std::ofstream& output, const String& temp_path, 
 	if (output.good()) {
 		output.close();
 
-		String finalFile = perfdata_path + "." + Convert::ToString((long)Utility::GetTime());
-		if (rename(temp_path.CStr(), finalFile.CStr()) < 0) {
-			BOOST_THROW_EXCEPTION(posix_error()
-			    << boost::errinfo_api_function("rename")
-			    << boost::errinfo_errno(errno)
-			    << boost::errinfo_file_name(temp_path));
+		if (Utility::PathExists(temp_path)) {
+			String finalFile = perfdata_path + "." + Convert::ToString((long)Utility::GetTime());
+			if (rename(temp_path.CStr(), finalFile.CStr()) < 0) {
+				BOOST_THROW_EXCEPTION(posix_error()
+				    << boost::errinfo_api_function("rename")
+				    << boost::errinfo_errno(errno)
+				    << boost::errinfo_file_name(temp_path));
+			}
 		}
 	}
 
