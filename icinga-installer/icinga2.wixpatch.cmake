5c4a833116654c9f742ffec3bbcd5e593c57f80b
Michael Friedrich <michael.friedrich@netways.de>
2016-07-06 17:11:21 +0200

    Windows: Remove requirement for KB2999226 (VS2015)
    refs #11739

--- icinga-installer/icinga2.wixpatch.cmake
+++ icinga-installer/icinga2.wixpatch.cmake
@@ -10,9 +10,12 @@
     <Binary Id="icinga2_installer" SourceFile="$<TARGET_FILE:icinga-installer>" />
     
     <InstallExecuteSequence>
+      <!-- Removed for builds with VS2013, kept intact for VS2015 --> 
+      <!--
       <Custom Action='CheckForUCRT' Before='LaunchConditions'>
         <![CDATA[Not REMOVE="ALL" AND Not PREVIOUSFOUND AND UCRTINSTALLED = ""]]>
       </Custom>
+      -->
       <Custom Action="XtraUpgradeNSIS" After="InstallInitialize">$CM_CP_sbin.icinga2_installer.exe&gt;2</Custom>
       <Custom Action="XtraInstall" Before="InstallFinalize">$CM_CP_sbin.icinga2_installer.exe&gt;2</Custom>
       <Custom Action="XtraUninstall" Before="RemoveExistingProducts">$CM_CP_sbin.icinga2_installer.exe=2</Custom>
@@ -33,6 +36,8 @@
             Value="LaunchIcinga2Wizard">WIXUI_EXITDIALOGOPTIONALCHECKBOX = 1 and NOT Installed</Publish>
     </UI>
 
+    <!-- Removed for builds with VS2013, kept intact for VS2015 --> 
+    <!--
     <Property Id="UCRTINSTALLED" Secure="yes">
       <DirectorySearch Id="searchSystem2" Path="[SystemFolder]" Depth="0">
         <FileSearch Id="UCRT_FileSearch"
@@ -42,5 +47,6 @@
     </Property>
 
     <CustomAction Id="CheckForUCRT" Error="Installation cannot continue because the Microsoft Universal C Runtime is not installed. Please see https://support.microsoft.com/en-us/kb/2999226 for more details." />
+    -->
   </CPackWiXFragment>
 </CPackWiXPatch>
