124c80beabf33829cffb28023ac8e2323f9d22ad
Michael Friedrich <michael.friedrich@netways.de>
2016-07-07 12:27:56 +0200

    Require DB IDO v1.14.1 for 2.5.0
    fixes #11967

--- lib/db_ido/dbconnection.hpp
+++ lib/db_ido/dbconnection.hpp
@@ -30,7 +30,7 @@
 #include <boost/thread/mutex.hpp>
 
 #define IDO_CURRENT_SCHEMA_VERSION "1.14.1"
-#define IDO_COMPAT_SCHEMA_VERSION "1.14.0"
+#define IDO_COMPAT_SCHEMA_VERSION "1.14.1"
 
 namespace icinga
 {
