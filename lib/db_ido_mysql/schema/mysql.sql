0b9046a0303c2a3a6416d220bc9553df58710830
Eric Lippmann <eric.lippmann@netways.de>
2016-11-17 12:22:00 +0100

    ido-mysql: Don't set the SQL mode to NO_AUTO_VALUE_ON_ZERO
    This mode is obsolete because timestamp columns now default to NULL instead of '0000-00...'.
    
    refs #10502

--- lib/db_ido_mysql/schema/mysql.sql
+++ lib/db_ido_mysql/schema/mysql.sql
@@ -6,14 +6,10 @@
 --
 -- -- --------------------------------------------------------
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
 
 --
 -- Database: icinga
