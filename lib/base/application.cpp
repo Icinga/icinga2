a5378e85a77f08826a2da4f16f1befad3be7e6ca
thoughtfix <thoughtfix@users.noreply.github.com>
2016-11-17 14:27:15 -0800

    Increase limit for large scale/high performance
    ulimit from 16x1024 to 64x1024

--- lib/base/application.cpp
+++ lib/base/application.cpp
@@ -171,7 +171,7 @@ void Application::SetResourceLimits(void)
 	rlimit rl;
 
 #	ifdef RLIMIT_NOFILE
-	rl.rlim_cur = 16 * 1024;
+	rl.rlim_cur = 64 * 1024;
 	rl.rlim_max = rl.rlim_cur;
 
 	if (setrlimit(RLIMIT_NOFILE, &rl) < 0)
@@ -181,7 +181,7 @@ void Application::SetResourceLimits(void)
 #	endif /* RLIMIT_NOFILE */
 
 #	ifdef RLIMIT_NPROC
-	rl.rlim_cur = 16 * 1024;
+	rl.rlim_cur = 64 * 1024;
 	rl.rlim_max = rl.rlim_cur;
 
 	if (setrlimit(RLIMIT_NPROC, &rl) < 0)
