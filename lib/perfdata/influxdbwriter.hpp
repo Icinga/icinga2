7627319dacacdf958fa93584f65c7cb8875ffd8e
Michael Friedrich <michael.friedrich@icinga.com>
2017-05-04 10:29:49 +0200

    InfluxdbWriter: Use a work queue for async message processing; add stats log/api
    refs #5219

--- lib/perfdata/influxdbwriter.hpp
+++ lib/perfdata/influxdbwriter.hpp
@@ -25,6 +25,9 @@
 #include "base/configobject.hpp"
 #include "base/tcpsocket.hpp"
 #include "base/timer.hpp"
+#include "base/ringbuffer.hpp"
+#include "base/workqueue.hpp"
+#include <boost/thread/mutex.hpp>
 #include <fstream>
 
 namespace icinga
@@ -41,23 +44,42 @@ public:
 	DECLARE_OBJECT(InfluxdbWriter);
 	DECLARE_OBJECTNAME(InfluxdbWriter);
 
+	InfluxdbWriter(void);
+
 	static void StatsFunc(const Dictionary::Ptr& status, const Array::Ptr& perfdata);
+
+	int GetTaskCount(RingBuffer::SizeType span) const;
 
 	virtual void ValidateHostTemplate(const Dictionary::Ptr& value, const ValidationUtils& utils) override;
 	virtual void ValidateServiceTemplate(const Dictionary::Ptr& value, const ValidationUtils& utils) override;
 
 protected:
+	virtual void OnConfigLoaded(void) override;
 	virtual void Start(bool runtimeCreated) override;
 
+	void IncreaseTaskCount(void);
+
 private:
+	WorkQueue m_WorkQueue;
 	Timer::Ptr m_FlushTimer;
-	Array::Ptr m_DataBuffer;
+	std::vector<String> m_DataBuffer;
+	boost::mutex m_DataBufferMutex;
+
+	mutable boost::mutex m_StatsMutex;
+	RingBuffer m_TaskStats;
+	int m_PendingTasks;
+	double m_PendingTasksTimestamp;
+
+	Timer::Ptr m_StatsLoggerTimer;
+	void StatsLoggerTimerHandler(void);
 
 	void CheckResultHandler(const Checkable::Ptr& checkable, const CheckResult::Ptr& cr);
 	void SendPerfdata(const Dictionary::Ptr& tmpl, const Checkable::Ptr& checkable, const CheckResult::Ptr& cr, double ts);
 	void SendMetric(const Dictionary::Ptr& tmpl, const String& label, const Dictionary::Ptr& fields, double ts);
 	void FlushTimeout(void);
 	void Flush(void);
+
+	void FlushHandler(const String& body);
 
 	static String FormatInteger(const int val);
 	static String FormatBoolean(const bool val);
@@ -66,6 +88,10 @@ private:
 	static String EscapeField(const String& str);
 
 	Stream::Ptr Connect(TcpSocket::Ptr& socket);
+
+	void AssertOnWorkQueue(void);
+
+	void ExceptionHandler(boost::exception_ptr exp);
 };
 
 }
