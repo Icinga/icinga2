ea24af659091a51f887c3df5b17844a008049605
Noah Hilverling <noah.hilverling@icinga.com>
2017-09-19 16:42:32 +0200

    SyslogLogger: Implement option to set syslog facility
    fixes #3964

--- lib/base/sysloglogger.cpp
+++ lib/base/sysloglogger.cpp
@@ -29,6 +29,55 @@ REGISTER_TYPE(SyslogLogger);
 
 REGISTER_STATSFUNCTION(SyslogLogger, &SyslogLogger::StatsFunc);
 
+INITIALIZE_ONCE(&SyslogLogger::StaticInitialize);
+
+std::map<String, int> SyslogLogger::m_FacilityMap;
+
+void SyslogLogger::StaticInitialize(void)
+{
+	ScriptGlobal::Set("FacilityAuth", "LOG_AUTH");
+	ScriptGlobal::Set("FacilityAuthPriv", "LOG_AUTHPRIV");
+	ScriptGlobal::Set("FacilityCron", "LOG_CRON");
+	ScriptGlobal::Set("FacilityDaemon", "LOG_DAEMON");
+	ScriptGlobal::Set("FacilityFtp", "LOG_FTP");
+	ScriptGlobal::Set("FacilityKern", "LOG_KERN");
+	ScriptGlobal::Set("FacilityLocal0", "LOG_LOCAL0");
+	ScriptGlobal::Set("FacilityLocal1", "LOG_LOCAL1");
+	ScriptGlobal::Set("FacilityLocal2", "LOG_LOCAL2");
+	ScriptGlobal::Set("FacilityLocal3", "LOG_LOCAL3");
+	ScriptGlobal::Set("FacilityLocal4", "LOG_LOCAL4");
+	ScriptGlobal::Set("FacilityLocal5", "LOG_LOCAL5");
+	ScriptGlobal::Set("FacilityLocal6", "LOG_LOCAL6");
+	ScriptGlobal::Set("FacilityLocal7", "LOG_LOCAL7");
+	ScriptGlobal::Set("FacilityLpr", "LOG_LPR");
+	ScriptGlobal::Set("FacilityMail", "LOG_MAIL");
+	ScriptGlobal::Set("FacilityNews", "LOG_NEWS");
+	ScriptGlobal::Set("FacilitySyslog", "LOG_SYSLOG");
+	ScriptGlobal::Set("FacilityUser", "LOG_USER");
+	ScriptGlobal::Set("FacilityUucp", "LOG_UUCP");
+
+	m_FacilityMap["LOG_AUTH"] = LOG_AUTH;
+	m_FacilityMap["LOG_AUTHPRIV"] = LOG_AUTHPRIV;
+	m_FacilityMap["LOG_CRON"] = LOG_CRON;
+	m_FacilityMap["LOG_DAEMON"] = LOG_DAEMON;
+	m_FacilityMap["LOG_FTP"] = LOG_FTP;
+	m_FacilityMap["LOG_KERN"] = LOG_KERN;
+	m_FacilityMap["LOG_LOCAL0"] = LOG_LOCAL0;
+	m_FacilityMap["LOG_LOCAL1"] = LOG_LOCAL1;
+	m_FacilityMap["LOG_LOCAL2"] = LOG_LOCAL2;
+	m_FacilityMap["LOG_LOCAL3"] = LOG_LOCAL3;
+	m_FacilityMap["LOG_LOCAL4"] = LOG_LOCAL4;
+	m_FacilityMap["LOG_LOCAL5"] = LOG_LOCAL5;
+	m_FacilityMap["LOG_LOCAL6"] = LOG_LOCAL6;
+	m_FacilityMap["LOG_LOCAL7"] = LOG_LOCAL7;
+	m_FacilityMap["LOG_LPR"] = LOG_LPR;
+	m_FacilityMap["LOG_MAIL"] = LOG_MAIL;
+	m_FacilityMap["LOG_NEWS"] = LOG_NEWS;
+	m_FacilityMap["LOG_SYSLOG"] = LOG_SYSLOG;
+	m_FacilityMap["LOG_USER"] = LOG_USER;
+	m_FacilityMap["LOG_UUCP"] = LOG_UUCP;
+}
+
 void SyslogLogger::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&)
 {
 	Dictionary::Ptr nodes = new Dictionary();
@@ -38,6 +87,33 @@ void SyslogLogger::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&)
 	}
 
 	status->Set("sysloglogger", nodes);
+}
+
+void SyslogLogger::OnConfigLoaded(void)
+{
+	ObjectImpl<SyslogLogger>::OnConfigLoaded();
+
+	String facilityString = GetFacility();
+
+	auto it = m_FacilityMap.find(facilityString);
+
+	if (it != m_FacilityMap.end())
+		m_Facility = it->second;
+	else
+		m_Facility = Convert::ToLong(facilityString);
+}
+
+void SyslogLogger::ValidateFacility(const String& value, const ValidationUtils& utils)
+{
+	ObjectImpl<SyslogLogger>::ValidateFacility(value, utils);
+
+	if (m_FacilityMap.find(value) == m_FacilityMap.end()) {
+		try {
+			Convert::ToLong(value);
+		} catch (const std::exception&) {
+			BOOST_THROW_EXCEPTION(ValidationError(this, boost::assign::list_of("facility"), "Invalid facility specified."));
+		}
+	}
 }
 
 /**
@@ -67,7 +143,7 @@ void SyslogLogger::ProcessLogEntry(const LogEntry& entry)
 			break;
 	}
 
-	syslog(severity | LOG_USER, "%s", entry.Message.CStr());
+	syslog(severity | m_Facility, "%s", entry.Message.CStr());
 }
 
 void SyslogLogger::Flush(void)
