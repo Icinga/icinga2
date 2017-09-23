43512a4f63e4745d9f08992beb63d69e34392e47
Markus Frosch <markus@lazyfrosch.de>
2017-09-23 15:16:00 +0200

    Improve documentation and Systemd config for TasksMax setting
    This also disables setting a value by default.
    
    Packaging should decide to install the config file:
    `/etc/systemd/system/icinga2.service.d`
    
    fixes #5611
    
    refs #5612
    
    Signed-off-by: Michael Friedrich <michael.friedrich@icinga.com>

--- etc/initsystem/icinga2.service.cmake
+++ etc/initsystem/icinga2.service.cmake
@@ -10,8 +10,17 @@ ExecStart=@CMAKE_INSTALL_FULL_SBINDIR@/icinga2 daemon -d -e ${ICINGA2_ERROR_LOG}
 PIDFile=@ICINGA2_RUNDIR@/icinga2/icinga2.pid
 ExecReload=@CMAKE_INSTALL_PREFIX@/lib/icinga2/safe-reload @ICINGA2_SYSCONFIGFILE@
 TimeoutStartSec=30m
-# Introduced in Systemd 226, defaults to 512. Icinga 2 requires more tasks (checks, notifications, etc.)
-DefaultTasksMax=infinity
+
+# Systemd >228 enforces a lower process number for services.
+# Depending on the distribution and Systemd version, this must
+# be explicitly raised. Packages will set the needed values
+# into /etc/systemd/system/icinga2.service.d/limits.conf
+#
+# Please check the troubleshooting documentation for further details.
+# The values below can be used as examples for customized service files.
+
+#TasksMax=infinity
+#LimitNPROC=62883
 
 [Install]
 WantedBy=multi-user.target
