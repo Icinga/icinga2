b83af363effc2333ae2f65753e9347d9428235b8
Noah Hilverling <noah.hilverling@icinga.com>
2018-01-17 10:25:42 +0100

    Add missing string escaping to InfluxDB-Writer
    fixes #5987

--- lib/perfdata/influxdbwriter.cpp
+++ lib/perfdata/influxdbwriter.cpp
@@ -321,6 +321,9 @@ String InfluxdbWriter::EscapeValue(const Value& value)
 	if (value.IsBoolean())
 		return value ? "true" : "false";
 
+	if (value.IsString())
+		return "\"" + EscapeKeyOrTagValue(value) + "\"";
+
 	return value;
 }
 
