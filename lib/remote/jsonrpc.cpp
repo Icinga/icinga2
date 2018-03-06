b172a67867d183ae29ee8a7fe0bffe5547cf6c65
Gunnar Beutner <gunnar.beutner@icinga.com>
2018-03-06 08:54:47 +0100

    Fix incorrect argument type for JsonRpc::SendMessage

--- lib/remote/jsonrpc.cpp
+++ lib/remote/jsonrpc.cpp
@@ -72,7 +72,7 @@ size_t JsonRpc::SendMessage(const Stream::Ptr& stream, const Dictionary::Ptr& me
 	return NetString::WriteStringToStream(stream, json);
 }
 
-StreamReadStatus JsonRpc::ReadMessage(const Stream::Ptr& stream, String *message, StreamReadContext& src, bool may_wait, size_t maxMessageLength)
+StreamReadStatus JsonRpc::ReadMessage(const Stream::Ptr& stream, String *message, StreamReadContext& src, bool may_wait, ssize_t maxMessageLength)
 {
 	String jsonString;
 	StreamReadStatus srs = NetString::ReadStringFromStream(stream, &jsonString, src, may_wait, maxMessageLength);
