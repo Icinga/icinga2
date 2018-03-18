662f81736452b68c76c1ac5e2b05a6dd3b81130a
Michael Insel <michael@insel.email>
2018-03-18 16:19:15 +0100

    Add option to windows installer to add global zones
    This implements the option to add global zones to the windows agent
    wizard.

---
+++ agent/windows-setup-agent/GlobalZonesInputBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Icinga
+{
+	public partial class GlobalZonesInputBox : Form
+	{
+		private ListView.ListViewItemCollection globalZonesItems;
+
+		public GlobalZonesInputBox(ListView.ListViewItemCollection globalZonesItems)
+		{
+			InitializeComponent();
+
+			this.globalZonesItems = globalZonesItems;
+		}
+
+		private void Warning(string message)
+		{
+			MessageBox.Show(this, message, Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+		}
+
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			if (txtGlobalZoneName.Text == "global-templates" || txtGlobalZoneName.Text == "director-global") {
+				Warning("This global zone is configured by default.");
+				return;
+			}
+
+			foreach (ListViewItem lvw in globalZonesItems) {
+				if (txtGlobalZoneName.Text == lvw.Text) {
+					Warning("This global zone is already defined.");
+					return;
+				}
+			}
+
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+	}
+}
