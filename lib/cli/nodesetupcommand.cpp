026359d4042879cabfacf12129e0566a49cae6bd
Jean Flach <jean-marcel.flach@icinga.com>
2018-05-02 17:42:30 +0200

    Rename "dont-disable-confd"
    Double negation is confusing. Also this would change the default
    behaviour and could lead to problems in automated environments.
    
    refs #4508

--- lib/cli/nodesetupcommand.cpp
+++ lib/cli/nodesetupcommand.cpp
@@ -67,7 +67,7 @@ void NodeSetupCommand::InitParameters(boost::program_options::options_descriptio
 		("accept-commands", "Accept commands from master")
 		("master", "Use setup for a master instance")
 		("global_zones", po::value<std::vector<std::string> >(), "The names of the additional global zones.")
-		("dont-disable-confd", "Disables the conf.d directory during the setup");
+		("disable-confd", "Disables the conf.d directory during the setup");
 
 	hiddenDesc.add_options()
 		("master_zone", po::value<std::string>(), "DEPRECATED: The name of the master zone")
@@ -245,7 +245,7 @@ int NodeSetupCommand::SetupMaster(const boost::program_options::variables_map& v
 	Log(LogInformation, "cli")
 		<< "Edit the api feature config file '" << apipath << "' and set a secure 'ticket_salt' attribute.";
 
-	if (!vm.count("dont-disable-confd")) {
+	if (vm.count("disable-confd")) {
 		/* Disable conf.d inclusion */
 		NodeUtility::UpdateConfiguration("\"conf.d\"", false, true);
 
@@ -570,7 +570,7 @@ int NodeSetupCommand::SetupNode(const boost::program_options::variables_map& vm,
 		Log(LogInformation, "cli", "Make sure to restart Icinga 2.");
 	}
 
-	if (!vm.count("dont-disable-confd")) {
+	if (vm.count("disable-confd")) {
 
 		/* Disable conf.d inclusion */
 		NodeUtility::UpdateConfiguration("\"conf.d\"", false, true);
