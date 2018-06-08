f6310acaf35585d7a0cedae018228913df90d107
Michael Friedrich <michael.friedrich@icinga.com>
2018-06-08 11:38:36 +0200

    Move stats into a separate file

---
+++ lib/redis/rediswriter-stats.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Icinga 2                                                                   *
+ * Copyright (C) 2012-2018 Icinga Development Team (https://www.icinga.com/)  *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software Foundation     *
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
+
+#include "redis/rediswriter.hpp"
+#include "base/json.hpp"
+#include "base/logger.hpp"
+#include "base/serializer.hpp"
+#include "base/statsfunction.hpp"
+#include "base/convert.hpp"
+
+using namespace icinga;
+
+Dictionary::Ptr RedisWriter::GetStats()
+{
+	//TODO: Figure out if more stats can be useful here.
+	Dictionary::Ptr statsFunctions = ScriptGlobal::Get("StatsFunctions", &Empty);
+
+	if (!statsFunctions)
+		Dictionary::Ptr();
+
+	Function::Ptr func = statsFunctions->Get("CIB");
+
+	Dictionary::Ptr status = new Dictionary();
+	Array::Ptr perfdata = new Array();
+
+	func->Invoke({ status, perfdata });
+
+//	String jsonStats = JsonEncode(status);
+
+	return status;
+}
+
