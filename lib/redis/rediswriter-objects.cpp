f7372d9227602ec3863e0572f4ff9221e2bc79f9
Michael Friedrich <michael.friedrich@icinga.com>
2018-07-10 16:35:32 +0200

    Simplify host_checksum

--- lib/redis/rediswriter-objects.cpp
+++ lib/redis/rediswriter-objects.cpp
@@ -209,7 +209,7 @@ void RedisWriter::SendConfigUpdate(const ConfigObject::Ptr& object, bool useTran
 			getGroup = &::GetServiceGroup;
 
 			/* Calculate the host_checksum */
-			checkSums->Set("host_checksum", GetIdentifier(service->GetHost()));
+			checkSums->Set("host_checksum", GetIdentifier(host));
 		} else {
 			groups = host->GetGroups();
 			getGroup = &::GetHostGroup;
