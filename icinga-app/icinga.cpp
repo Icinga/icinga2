Icinga/icinga2

commit e678fa1aa57b079ccc75dfac4086b48bcf809b11

Refactor Application::*Const()

icinga-app/icinga.cpp
@@ -23,6 +23,7 @@
 #include "config/configitembuilder.hpp"
 #include "config/expression.hpp"
 #include "base/application.hpp"
+#include "base/configuration.hpp"
 #include "base/logger.hpp"
 #include "base/timer.hpp"
 #include "base/utility.hpp"
@@ -101,7 +102,7 @@ static void HandleLegacyDefines()
 	String dataPrefix = Utility::GetIcingaDataPath();
 #endif /* _WIN32 */
 
-	Value localStateDir = Application::GetConst("LocalStateDir");
+	Value localStateDir = Configuration::LocalStateDir;
 
 	if (!localStateDir.IsEmpty()) {
 		Log(LogWarning, "icinga-app")
@@ -110,51 +111,51 @@ static void HandleLegacyDefines()
 			<< " For compatibility reasons, these are now set based on the 'LocalStateDir' constant.";
 
 #ifdef _WIN32
-		ScriptGlobal::Set("DataDir", localStateDir + "\\lib\\icinga2");
-		ScriptGlobal::Set("LogDir", localStateDir + "\\log\\icinga2");
-		ScriptGlobal::Set("CacheDir", localStateDir + "\\cache\\icinga2");
-		ScriptGlobal::Set("SpoolDir", localStateDir + "\\spool\\icinga2");
+		Configuration::DataDir = localStateDir + "\\lib\\icinga2";
+		Configuration::LogDir = localStateDir + "\\log\\icinga2";
+		Configuration::CacheDir = localStateDir + "\\cache\\icinga2";
+		Configuration::SpoolDir = localStateDir + "\\spool\\icinga2";
 	} else {
-		ScriptGlobal::Set("LocalStateDir", dataPrefix + "\\var");
+		Configuration::LocalStateDir = dataPrefix + "\\var";
 #else /* _WIN32 */
-		ScriptGlobal::Set("DataDir", localStateDir + "/lib/icinga2");
-		ScriptGlobal::Set("LogDir", localStateDir + "/log/icinga2");
-		ScriptGlobal::Set("CacheDir", localStateDir + "/cache/icinga2");
-		ScriptGlobal::Set("SpoolDir", localStateDir + "/spool/icinga2");
+		Configuration::DataDir = localStateDir + "/lib/icinga2";
+		Configuration::LogDir = localStateDir + "/log/icinga2";
+		Configuration::CacheDir = localStateDir + "/cache/icinga2";
+		Configuration::SpoolDir = localStateDir + "/spool/icinga2";
 	} else {
-		ScriptGlobal::Set("LocalStateDir", ICINGA_LOCALSTATEDIR);
+		Configuration::LocalStateDir = ICINGA_LOCALSTATEDIR;
 #endif /* _WIN32 */
 	}
 
-	Value sysconfDir = Application::GetConst("SysconfDir");
+	Value sysconfDir = Configuration::SysconfDir;
 	if (!sysconfDir.IsEmpty()) {
 		Log(LogWarning, "icinga-app")
 			<< "Please do not set the deprecated 'Sysconfdir' constant, use the 'ConfigDir' constant instead! For compatibility reasons, their value is set based on the 'SysconfDir' constant.";
 
 #ifdef _WIN32
-		ScriptGlobal::Set("ConfigDir", sysconfDir + "\\icinga2");
+		Configuration::ConfigDir = sysconfDir + "\\icinga2";
 	} else {
-		ScriptGlobal::Set("SysconfDir", dataPrefix + "\\etc");
+		Configuration::SysconfDir = dataPrefix + "\\etc";
 #else /* _WIN32 */
-		ScriptGlobal::Set("ConfigDir", sysconfDir + "/icinga2");
+		Configuration::ConfigDir = sysconfDir + "/icinga2";
 	} else {
-		ScriptGlobal::Set("SysconfDir", ICINGA_SYSCONFDIR);
+		Configuration::SysconfDir = ICINGA_SYSCONFDIR;
 #endif /* _WIN32 */
 	}
 
-	Value runDir = Application::GetConst("RunDir");
+	Value runDir = Configuration::RunDir;
 	if (!runDir.IsEmpty()) {
 		Log(LogWarning, "icinga-app")
 			<< "Please do not set the deprecated 'RunDir' constant, use the 'InitRunDir' constant instead! For compatiblity reasons, their value is set based on the 'RunDir' constant.";
 
 #ifdef _WIN32
-		ScriptGlobal::Set("InitRunDir", runDir + "\\icinga2");
+		Configuration::InitRunDir = runDir + "\\icinga2";
 	} else {
-		ScriptGlobal::Set("RunDir", dataPrefix + "\\var\\run");
+		Configuration::RunDir = dataPrefix + "\\var\\run";
 #else /* _WIN32 */
-		ScriptGlobal::Set("InitRunDir", runDir + "/icinga2");
+		Configuration::InitRunDir = runDir + "/icinga2";
 	} else {
-		ScriptGlobal::Set("RunDir", ICINGA_RUNDIR);
+		Configuration::RunDir = ICINGA_RUNDIR;
 #endif /* _WIN32 */
 	}
 }
@@ -199,43 +200,43 @@ static int Main()
 	String dataPrefix = Utility::GetIcingaDataPath();
 
 	if (!binaryPrefix.IsEmpty() && !dataPrefix.IsEmpty()) {
-		Application::DeclareConst("PrefixDir", binaryPrefix);
-		Application::DeclareConst("ProgramData", dataPrefix);
+		Configuration::PrefixDir = binaryPrefix;
+		Configuration::ProgramData = dataPrefix;
 
-		Application::DeclareConst("ConfigDir", dataPrefix + "\\etc\\icinga2");
+		Configuration::ConfigDir = dataPrefix + "\\etc\\icinga2";
 
-		Application::DeclareConst("DataDir", dataPrefix + "\\var\\lib\\icinga2");
-		Application::DeclareConst("LogDir", dataPrefix + "\\var\\log\\icinga2");
-		Application::DeclareConst("CacheDir", dataPrefix + "\\var\\cache\\icinga2");
-		Application::DeclareConst("SpoolDir", dataPrefix + "\\var\\spool\\icinga2");
+		Configuration::DataDir =  dataPrefix + "\\var\\lib\\icinga2";
+		Configuration::LogDir = dataPrefix + "\\var\\log\\icinga2";
+		Configuration::CacheDir = dataPrefix + "\\var\\cache\\icinga2";
+		Configuration::SpoolDir = dataPrefix + "\\var\\spool\\icinga2";
 
 		/* Internal constants. */
-		Application::DeclareConst("PkgDataDir", binaryPrefix + "\\share\\icinga2");
-		Application::DeclareConst("IncludeConfDir", binaryPrefix + "\\share\\icinga2\\include");
+		Configuration::PkgDataDir = binaryPrefix + "\\share\\icinga2";
+		Configuration::IncludeConfDir = binaryPrefix + "\\share\\icinga2\\include";
 	} else {
 		Log(LogWarning, "icinga-app", "Registry key could not be read. Falling back to built-in paths.");
 
 #endif /* _WIN32 */
-		Application::DeclareConst("ConfigDir", ICINGA_CONFIGDIR);
+		Configuration::ConfigDir = ICINGA_CONFIGDIR;
 
-		Application::DeclareConst("DataDir", ICINGA_DATADIR);
-		Application::DeclareConst("LogDir", ICINGA_LOGDIR);
-		Application::DeclareConst("CacheDir", ICINGA_CACHEDIR);
-		Application::DeclareConst("SpoolDir", ICINGA_SPOOLDIR);
+		Configuration::DataDir = ICINGA_DATADIR;
+		Configuration::LogDir = ICINGA_LOGDIR;
+		Configuration::CacheDir = ICINGA_CACHEDIR;
+		Configuration::SpoolDir = ICINGA_SPOOLDIR;
 
-		Application::DeclareConst("PrefixDir", ICINGA_PREFIX);
+		Configuration::PrefixDir = ICINGA_PREFIX;
 
 		/* Internal constants. */
-		Application::DeclareConst("PkgDataDir", ICINGA_PKGDATADIR);
-		Application::DeclareConst("IncludeConfDir", ICINGA_INCLUDECONFDIR);
+		Configuration::PkgDataDir = ICINGA_PKGDATADIR;
+		Configuration::IncludeConfDir = ICINGA_INCLUDECONFDIR;
 
-		Application::DeclareConst("InitRunDir", ICINGA_INITRUNDIR);
+		Configuration::InitRunDir = ICINGA_INITRUNDIR;
 
 #ifdef _WIN32
 	}
 #endif /* _WIN32 */
 
-	Application::DeclareConst("ZonesDir", Application::GetConst("ConfigDir") + "/zones.d");
+	Configuration::ZonesDir = Configuration::ConfigDir + "/zones.d";
 
 	String icingaUser = Utility::GetFromEnvironment("ICINGA2_USER");
 	if (icingaUser.IsEmpty())
@@ -245,17 +246,17 @@ static int Main()
 	if (icingaGroup.IsEmpty())
 		icingaGroup = ICINGA_GROUP;
 
-	Application::DeclareConst("RunAsUser", icingaUser);
-	Application::DeclareConst("RunAsGroup", icingaGroup);
+	Configuration::RunAsUser = icingaUser;
+	Configuration::RunAsGroup = icingaGroup;
 
 	if (!autocomplete) {
 #ifdef RLIMIT_NOFILE
 		String rLimitFiles = Utility::GetFromEnvironment("ICINGA2_RLIMIT_FILES");
 		if (rLimitFiles.IsEmpty())
-			Application::DeclareConst("RLimitFiles", Application::GetDefaultRLimitFiles());
+			Configuration::RLimitFiles = Application::GetDefaultRLimitFiles();
 		else {
 			try {
-				Application::DeclareConst("RLimitFiles", Convert::ToLong(rLimitFiles));
+				Configuration::RLimitFiles = Convert::ToLong(rLimitFiles);
 			} catch (const std::invalid_argument& ex) {
 				std::cout
 					<< "Error setting \"ICINGA2_RLIMIT_FILES\": " << ex.what() << '\n';
@@ -267,10 +268,10 @@ static int Main()
 #ifdef RLIMIT_NPROC
 		String rLimitProcesses = Utility::GetFromEnvironment("ICINGA2_RLIMIT_PROCESSES");
 		if (rLimitProcesses.IsEmpty())
-			Application::DeclareConst("RLimitProcesses", Application::GetDefaultRLimitProcesses());
+			Configuration::RLimitProcesses = Application::GetDefaultRLimitProcesses();
 		else {
 			try {
-				Application::DeclareConst("RLimitProcesses", Convert::ToLong(rLimitProcesses));
+				Configuration::RLimitProcesses = Convert::ToLong(rLimitProcesses);
 			} catch (const std::invalid_argument& ex) {
 				std::cout
 					<< "Error setting \"ICINGA2_RLIMIT_PROCESSES\": " << ex.what() << '\n';
@@ -282,10 +283,10 @@ static int Main()
 #ifdef RLIMIT_STACK
 		String rLimitStack = Utility::GetFromEnvironment("ICINGA2_RLIMIT_STACK");
 		if (rLimitStack.IsEmpty())
-			Application::DeclareConst("RLimitStack", Application::GetDefaultRLimitStack());
+			Configuration::RLimitStack = Application::GetDefaultRLimitStack();
 		else {
 			try {
-				Application::DeclareConst("RLimitStack", Convert::ToLong(rLimitStack));
+				Configuration::RLimitStack = Convert::ToLong(rLimitStack);
 			} catch (const std::invalid_argument& ex) {
 				std::cout
 					<< "Error setting \"ICINGA2_RLIMIT_STACK\": " << ex.what() << '\n';
@@ -295,10 +296,9 @@ static int Main()
 #endif /* RLIMIT_STACK */
 	}
 
-	Application::DeclareConst("Concurrency", std::thread::hardware_concurrency());
-	Application::DeclareConst("MaxConcurrentChecks", Application::GetDefaultMaxConcurrentChecks());
+	ScriptGlobal::Set("MaxConcurrentChecks", Application::GetDefaultMaxConcurrentChecks());
 
-	ScriptGlobal::Set("AttachDebugger", false);
+	ScriptGlobal::Set("Environment", "production");
 
 	ScriptGlobal::Set("System.PlatformKernel", Utility::GetPlatformKernel(), true);
 	ScriptGlobal::Set("System.PlatformKernelVersion", Utility::GetPlatformKernelVersion(), true);
@@ -360,7 +360,7 @@ static int Main()
 	std::ifstream userFile;
 
 	/* The implicit string assignment is needed for Windows builds. */
-	String configDir = Application::GetConst("ConfigDir");
+	String configDir = Configuration::ConfigDir;
 	userFile.open(configDir + "/user");
 
 	if (userFile && command && !Application::IsProcessElevated()) {
@@ -462,19 +462,21 @@ static int Main()
 		}
 	}
 
+	Configuration::SetReadOnly(true);
+
 	/* Ensure that all defined constants work in the way we expect them. */
 	HandleLegacyDefines();
 
 	if (vm.count("script-debugger"))
 		Application::SetScriptDebuggerEnabled(true);
 
-	Application::DeclareConst("StatePath", Application::GetConst("DataDir") + "/icinga2.state");
-	Application::DeclareConst("ModAttrPath", Application::GetConst("DataDir") + "/modified-attributes.conf");
-	Application::DeclareConst("ObjectsPath", Application::GetConst("CacheDir") + "/icinga2.debug");
-	Application::DeclareConst("VarsPath", Application::GetConst("CacheDir") + "/icinga2.vars");
-	Application::DeclareConst("PidPath", Application::GetConst("InitRunDir") + "/icinga2.pid");
+	Configuration::StatePath = Configuration::DataDir + "/icinga2.state";
+	Configuration::ModAttrPath = Configuration::DataDir + "/modified-attributes.conf";
+	Configuration::ObjectsPath = Configuration::CacheDir + "/icinga2.debug";
+	Configuration::VarsPath = Configuration::CacheDir + "/icinga2.vars";
+	Configuration::PidPath = Configuration::InitRunDir + "/icinga2.pid";
 
-	ConfigCompiler::AddIncludeSearchDir(Application::GetConst("IncludeConfDir"));
+	ConfigCompiler::AddIncludeSearchDir(Configuration::IncludeConfDir);
 
 	if (!autocomplete && vm.count("include")) {
 		for (const String& includePath : vm["include"].as<std::vector<std::string> >()) {
@@ -584,8 +586,8 @@ static int Main()
 				return 0;
 			}
 		} else if (command && command->GetImpersonationLevel() == ImpersonateIcinga) {
-			String group = Application::GetConst("RunAsGroup");
-			String user = Application::GetConst("RunAsUser");
+			String group = Configuration::RunAsGroup;
+			String user = Configuration::RunAsUser;
 
 			errno = 0;
 			struct group *gr = getgrnam(group.CStr());

lib/base/CMakeLists.txt
@@ -17,6 +17,7 @@
 
 mkclass_target(application.ti application-ti.cpp application-ti.hpp)
 mkclass_target(configobject.ti configobject-ti.cpp configobject-ti.hpp)
+mkclass_target(configuration.ti configuration-ti.cpp configuration-ti.hpp)
 mkclass_target(datetime.ti datetime-ti.cpp datetime-ti.hpp)
 mkclass_target(filelogger.ti filelogger-ti.cpp filelogger-ti.hpp)
 mkclass_target(function.ti function-ti.cpp function-ti.hpp)
@@ -33,6 +34,7 @@ set(base_SOURCES
   boolean.cpp boolean.hpp boolean-script.cpp
   configobject.cpp configobject.hpp configobject-ti.hpp configobject-script.cpp
   configtype.cpp configtype.hpp
+  configuration.cpp configuration.hpp configuration-ti.hpp
   configwriter.cpp configwriter.hpp
   console.cpp console.hpp
   context.cpp context.hpp

lib/base/application.cpp
@@ -168,7 +168,7 @@ void Application::SetResourceLimits()
 	rlimit rl;
 
 #	ifdef RLIMIT_NOFILE
-	rlim_t fileLimit = GetConst("RLimitFiles");
+	rlim_t fileLimit = Configuration::RLimitFiles;
 
 	if (fileLimit != 0) {
 		if (fileLimit < GetDefaultRLimitFiles()) {
@@ -189,7 +189,7 @@ void Application::SetResourceLimits()
 	}
 
 #	ifdef RLIMIT_NPROC
-	rlim_t processLimit = GetConst("RLimitProcesses");
+	rlim_t processLimit = Configuration::RLimitProcesses;
 
 	if (processLimit != 0) {
 		if (processLimit < GetDefaultRLimitProcesses()) {
@@ -228,7 +228,7 @@ void Application::SetResourceLimits()
 
 	rlim_t stackLimit;
 
-	stackLimit = GetConst("RLimitStack");
+	stackLimit = Configuration::RLimitStack;
 
 	if (stackLimit != 0) {
 		if (stackLimit < GetDefaultRLimitStack()) {
@@ -549,30 +549,32 @@ void Application::DisplayInfoMessage(std::ostream& os, bool skipVersion)
 		<< "  Kernel version: " << Utility::GetPlatformKernelVersion() << "\n"
 		<< "  Architecture: " << Utility::GetPlatformArchitecture() << "\n";
 
+	Namespace::Ptr systemNS = ScriptGlobal::Get("System");
+
 	os << "\nBuild information:\n"
-		<< "  Compiler: " << ScriptGlobal::Get("BuildCompilerName") << " " << ScriptGlobal::Get("BuildCompilerVersion") << "\n"
-		<< "  Build host: " << ScriptGlobal::Get("BuildHostName") << "\n";
+		<< "  Compiler: " << systemNS->Get("BuildCompilerName") << " " << systemNS->Get("BuildCompilerVersion") << "\n"
+		<< "  Build host: " << systemNS->Get("BuildHostName") << "\n";
 
 	os << "\nApplication information:\n"
 		<< "\nGeneral paths:\n"
-		<< "  Config directory: " << GetConst("ConfigDir") << "\n"
-		<< "  Data directory: " << GetConst("DataDir") << "\n"
-		<< "  Log directory: " << GetConst("LogDir") << "\n"
-		<< "  Cache directory: " << GetConst("CacheDir") << "\n"
-		<< "  Spool directory: " << GetConst("SpoolDir") << "\n"
-		<< "  Run directory: " << GetConst("InitRunDir") << "\n"
+		<< "  Config directory: " << Configuration::ConfigDir << "\n"
+		<< "  Data directory: " << Configuration::DataDir << "\n"
+		<< "  Log directory: " << Configuration::LogDir << "\n"
+		<< "  Cache directory: " << Configuration::CacheDir << "\n"
+		<< "  Spool directory: " << Configuration::SpoolDir << "\n"
+		<< "  Run directory: " << Configuration::InitRunDir << "\n"
 		<< "\nOld paths (deprecated):\n"
-		<< "  Installation root: " << GetConst("PrefixDir") << "\n"
-		<< "  Sysconf directory: " << GetConst("SysconfDir") << "\n"
-		<< "  Run directory (base): " << GetConst("RunDir") << "\n"
-		<< "  Local state directory: " << GetConst("LocalStateDir") << "\n"
+		<< "  Installation root: " << Configuration::PrefixDir << "\n"
+		<< "  Sysconf directory: " << Configuration::SysconfDir << "\n"
+		<< "  Run directory (base): " << Configuration::RunDir << "\n"
+		<< "  Local state directory: " << Configuration::LocalStateDir << "\n"
 		<< "\nInternal paths:\n"
-		<< "  Package data directory: " << GetConst("PkgDataDir") << "\n"
-		<< "  State path: " << GetConst("StatePath") << "\n"
-		<< "  Modified attributes path: " << GetConst("ModAttrPath") << "\n"
-		<< "  Objects path: " << GetConst("ObjectsPath") << "\n"
-		<< "  Vars path: " << GetConst("VarsPath") << "\n"
-		<< "  PID path: " << GetConst("PidPath") << "\n";
+		<< "  Package data directory: " << Configuration::PkgDataDir << "\n"
+		<< "  State path: " << Configuration::StatePath << "\n"
+		<< "  Modified attributes path: " << Configuration::ModAttrPath << "\n"
+		<< "  Objects path: " << Configuration::ObjectsPath << "\n"
+		<< "  Vars path: " << Configuration::VarsPath << "\n"
+		<< "  PID path: " << Configuration::PidPath << "\n";
 
 }
 
@@ -590,7 +592,7 @@ void Application::DisplayBugMessage(std::ostream& os)
 
 String Application::GetCrashReportFilename()
 {
-	return GetConst("LogDir") + "/crash/report." + Convert::ToString(Utility::GetTime());
+	return Configuration::LogDir + "/crash/report." + Convert::ToString(Utility::GetTime());
 }
 
 
@@ -740,7 +742,7 @@ void Application::SigUsr2Handler(int)
 	 */
 	Application::Ptr instance = GetInstance();
 	try {
-		instance->UpdatePidFile(GetConst("PidPath"), m_ReloadProcess);
+		instance->UpdatePidFile(Configuration::PidPath, m_ReloadProcess);
 	} catch (const std::exception&) {
 		/* abort restart */
 		Log(LogCritical, "Application", "Cannot update PID file. Aborting restart operation.");
@@ -783,7 +785,7 @@ void Application::SigAbrtHandler(int)
 		}
 	}
 
-	bool interactive_debugger = Convert::ToBool(ScriptGlobal::Get("AttachDebugger"));
+	bool interactive_debugger = Configuration::AttachDebugger;
 
 	if (!interactive_debugger) {
 		std::ofstream ofs;
@@ -893,7 +895,7 @@ void Application::ExceptionHandler()
 		}
 	}
 
-	bool interactive_debugger = Convert::ToBool(ScriptGlobal::Get("AttachDebugger"));
+	bool interactive_debugger = Configuration::AttachDebugger;
 
 	if (!interactive_debugger) {
 		std::ofstream ofs;
@@ -1012,10 +1014,10 @@ int Application::Run()
 #endif /* _WIN32 */
 
 	try {
-		UpdatePidFile(GetConst("PidPath"));
+		UpdatePidFile(Configuration::PidPath);
 	} catch (const std::exception&) {
 		Log(LogCritical, "Application")
-			<< "Cannot update PID file '" << GetConst("PidPath") << "'. Aborting.";
+			<< "Cannot update PID file '" << Configuration::PidPath << "'. Aborting.";
 		return EXIT_FAILURE;
 	}
 
@@ -1097,7 +1099,7 @@ void Application::ClosePidFile(bool unlink)
 
 	if (m_PidFile) {
 		if (unlink) {
-			String pidpath = GetConst("PidPath");
+			String pidpath = Configuration::PidPath;
 			::unlink(pidpath.CStr());
 		}
 
@@ -1165,39 +1167,6 @@ pid_t Application::ReadPidFile(const String& filename)
 	return runningpid;
 }
 
-/**
- * Declares a const with ScriptGlobal
- *
- * @param name The const name.
- * @param value The new value.
- */
-void Application::DeclareConst(const String& name, const Value& value)
-{
-	if (!ScriptGlobal::Exists(name))
-		ScriptGlobal::Set(name, value);
-}
-
-/**
- * Returns the value of a const from ScriptGlobal
- *
- * @param name The const name.
- */
-Value Application::GetConst(const String& name)
-{
-	return GetConst(name, Empty);
-}
-
-/**
- * Returns the value of a const from ScriptGlobal with default value
- *
- * @param name The const name.
- * @param def  The default value.
- */
-Value Application::GetConst(const String& name, Value defaultValue)
-{
-	return ScriptGlobal::Get(name, &defaultValue);
-}
-
 int Application::GetDefaultRLimitFiles()
 {
 	return 16 * 1024;
@@ -1211,17 +1180,6 @@ int Application::GetDefaultRLimitProcesses()
 int Application::GetDefaultRLimitStack()
 {
 	return 256 * 1024;
-}
-
-/**
- * Retrieves the concurrency level.
- *
- * @returns The concurrency level.
- */
-int Application::GetConcurrency()
-{
-	Value defaultConcurrency = std::thread::hardware_concurrency();
-	return ScriptGlobal::Get("Concurrency", &defaultConcurrency);
 }
 
 /**

lib/base/application.hpp
@@ -23,6 +23,7 @@
 #include "base/i2-base.hpp"
 #include "base/application-ti.hpp"
 #include "base/logger.hpp"
+#include "base/configuration.hpp"
 #include <iosfwd>
 
 namespace icinga
@@ -85,10 +86,6 @@ public:
 
 	static String GetExePath(const String& argv0);
 
-	static void DeclareConst(const String& name, const Value& value);
-	static Value GetConst(const String& name);
-	static Value GetConst(const String& name, Value defaultValue);
-
 #ifdef _WIN32
 	static bool IsProcessElevated();
 #endif /* _WIN32 */
@@ -97,7 +94,6 @@ public:
 	static int GetDefaultRLimitProcesses();
 	static int GetDefaultRLimitStack();
 
-	static int GetConcurrency();
 	static int GetMaxConcurrentChecks();
 	static int GetDefaultMaxConcurrentChecks();
 	static void SetMaxConcurrentChecks(int maxChecks);

lib/base/configobject.cpp
@@ -571,7 +571,7 @@ void ConfigObject::RestoreObjects(const String& filename, int attributeTypes)
 
 	unsigned long restored = 0;
 
-	WorkQueue upq(25000, Application::GetConcurrency());
+	WorkQueue upq(25000, Configuration::Concurrency);
 	upq.SetName("ConfigObject::RestoreObjects");
 
 	String message;

commit 7abfe13a00760382363c49b3c04597822cd38a6b

Remove default environment, regression from e678fa1aa5
refs #6512
fixes #6694

icinga-app/icinga.cpp
@@ -301,8 +301,6 @@ static int Main()
 
 	ScriptGlobal::Set("MaxConcurrentChecks", Application::GetDefaultMaxConcurrentChecks());
 
-	ScriptGlobal::Set("Environment", "production");
-
 	ScriptGlobal::Set("System.PlatformKernel", Utility::GetPlatformKernel(), true);
 	ScriptGlobal::Set("System.PlatformKernelVersion", Utility::GetPlatformKernelVersion(), true);
 	ScriptGlobal::Set("System.PlatformName", Utility::GetPlatformName(), true);