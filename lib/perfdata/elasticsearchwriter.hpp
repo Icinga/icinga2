59389f564c89cffa1c8b90c898de35add6c2587a
Michael Friedrich <michael.friedrich@icinga.com>
2018-10-24 13:20:03 +0200

    Implement HA functionality for Elasticsearch feature

--- lib/perfdata/elasticsearchwriter.hpp
+++ lib/perfdata/elasticsearchwriter.hpp
@@ -41,8 +41,8 @@ public:
 
 protected:
 	void OnConfigLoaded() override;
-	void Start(bool runtimeCreated) override;
-	void Stop(bool runtimeRemoved) override;
+	void Resume() override;
+	void Pause() override;
 
 private:
 	String m_EventPrefix;
