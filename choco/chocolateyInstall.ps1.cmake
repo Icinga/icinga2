9f82faa439a9360b951081f5f9de611ce62170af
Markus Frosch <markus.frosch@icinga.com>
2019-04-04 12:55:52 +0200

    cmake: Use the actual GIT_VERSION for ICINGA2_VERSION
    And use a safe version with dots only for Windows.

--- choco/chocolateyInstall.ps1.cmake
+++ choco/chocolateyInstall.ps1.cmake
@@ -1,7 +1,7 @@
 ﻿$packageName = 'icinga2'
 $installerType = 'msi'
-$url32 = 'https://packages.icinga.com/windows/Icinga2-v${SPEC_VERSION}-x86.msi'
-$url64 = 'https://packages.icinga.com/windows/Icinga2-v${SPEC_VERSION}-x86_64.msi'
+$url32 = 'https://packages.icinga.com/windows/Icinga2-v${ICINGA2_VERSION_SAFE}-x86.msi'
+$url64 = 'https://packages.icinga.com/windows/Icinga2-v${ICINGA2_VERSION_SAFE}-x86_64.msi'
 $silentArgs = '/qn /norestart'
 $validExitCodes = @(0)
 
