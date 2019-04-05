f177d8786d98b2752d4c9ba3ecf7204245176e38
Michael Friedrich <michael.friedrich@icinga.com>
2019-04-05 15:08:09 +0200

    HttpServerConnection: Log the user agent field for new requests too
    refs #7041

--- lib/remote/httpserverconnection.cpp
+++ lib/remote/httpserverconnection.cpp
@@ -489,7 +489,9 @@ void HttpServerConnection::ProcessMessages(boost::asio::yield_context yc)
 			Log(LogInformation, "HttpServerConnection")
 				<< "Request: " << request.method_string() << ' ' << request.target()
 				<< " (from " << m_PeerAddress
-				<< "), user: " << (authenticatedUser ? authenticatedUser->GetName() : "<unauthenticated>") << ')';
+				<< "), user: " << (authenticatedUser ? authenticatedUser->GetName() : "<unauthenticated>")
+				<< ", agent: " << request[http::field::user_agent] << ")."; //operator[] - Returns the value for a field, or "" if it does not exist.
+
 
 			if (!HandleAccessControl(*m_Stream, request, response, yc)) {
 				break;
