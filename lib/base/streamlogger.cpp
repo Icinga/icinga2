f551666a7d44fef4f2466073d55b3dedc8964e9b
Alexander A. Klimov <alexander.klimov@icinga.com>
2019-04-18 12:23:50 +0200

    StreamLogger#BindStream(): set #m_FlushLogTimer only if needed
    refs #6737
    
    (cherry picked from commit 622f68412440d4fa3ac1b347a929087073bc24f9)

--- lib/base/streamlogger.cpp
+++ lib/base/streamlogger.cpp
@@ -72,10 +72,12 @@ void StreamLogger::BindStream(std::ostream *stream, bool ownsStream)
 	m_Stream = stream;
 	m_OwnsStream = ownsStream;
 
-	m_FlushLogTimer = new Timer();
-	m_FlushLogTimer->SetInterval(1);
-	m_FlushLogTimer->OnTimerExpired.connect(std::bind(&StreamLogger::FlushLogTimerHandler, this));
-	m_FlushLogTimer->Start();
+	if (!m_FlushLogTimer) {
+		m_FlushLogTimer = new Timer();
+		m_FlushLogTimer->SetInterval(1);
+		m_FlushLogTimer->OnTimerExpired.connect(std::bind(&StreamLogger::FlushLogTimerHandler, this));
+		m_FlushLogTimer->Start();
+	}
 }
 
 /**
