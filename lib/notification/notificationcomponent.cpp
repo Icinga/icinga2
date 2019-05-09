c3df943ceb3f8882b5d4a2bef97285117189ad9c
Jean Flach <jean.flach@icinga.com>
2019-05-09 15:35:47 +0200

    Clear NotificationComponent and listen to events

--- lib/notification/notificationcomponent.cpp
+++ lib/notification/notificationcomponent.cpp
@@ -2,31 +2,11 @@
 
 #include "notification/notificationcomponent.hpp"
 #include "notification/notificationcomponent-ti.cpp"
-#include "icinga/service.hpp"
-#include "icinga/icingaapplication.hpp"
-#include "base/configtype.hpp"
-#include "base/objectlock.hpp"
-#include "base/logger.hpp"
-#include "base/utility.hpp"
-#include "base/exception.hpp"
-#include "base/statsfunction.hpp"
+
 
 using namespace icinga;
 
 REGISTER_TYPE(NotificationComponent);
-
-REGISTER_STATSFUNCTION(NotificationComponent, &NotificationComponent::StatsFunc);
-
-void NotificationComponent::StatsFunc(const Dictionary::Ptr& status, const Array::Ptr&)
-{
-	DictionaryData nodes;
-
-	for (const NotificationComponent::Ptr& notification_component : ConfigType::GetObjectsByType<NotificationComponent>()) {
-		nodes.emplace_back(notification_component->GetName(), 1); //add more stats
-	}
-
-	status->Set("notificationcomponent", new Dictionary(std::move(nodes)));
-}
 
 /**
  * Starts the component.
@@ -38,13 +18,7 @@ void NotificationComponent::Start(bool runtimeCreated)
 	Log(LogInformation, "NotificationComponent")
 		<< "'" << GetName() << "' started.";
 
-	Checkable::OnNotificationsRequested.connect(std::bind(&NotificationComponent::SendNotificationsHandler, this, _1,
-		_2, _3, _4, _5));
-
-	m_NotificationTimer = new Timer();
-	m_NotificationTimer->SetInterval(5);
-	m_NotificationTimer->OnTimerExpired.connect(std::bind(&NotificationComponent::NotificationTimerHandler, this));
-	m_NotificationTimer->Start();
+	Checkable::OnStateChange.connect(std::bind(&NotificationComponent::StateChangeHandler, this, _1, _2, _3));
 }
 
 void NotificationComponent::Stop(bool runtimeRemoved)
@@ -55,89 +29,25 @@ void NotificationComponent::Stop(bool runtimeRemoved)
 	ObjectImpl<NotificationComponent>::Stop(runtimeRemoved);
 }
 
-/**
- * Periodically sends notifications.
- *
- * @param - Event arguments for the timer.
- */
-void NotificationComponent::NotificationTimerHandler()
-{
-	double now = Utility::GetTime();
+void NotificationComponent::StateChangeHandler(const Checkable::Ptr& checkable, const CheckResult::Ptr& cr, StateType type) {
+	Host::Ptr host;
+	Service::Ptr service;
+	tie(host, service) = GetHostService(checkable);
 
-	/* Function already checks whether 'api' feature is enabled. */
-	Endpoint::Ptr myEndpoint = Endpoint::GetLocalEndpoint();
-
-	for (const Notification::Ptr& notification : ConfigType::GetObjectsByType<Notification>()) {
-		if (!notification->IsActive())
-			continue;
-
-		String notificationName = notification->GetName();
-
-		/* Skip notification if paused, in a cluster setup & HA feature is enabled. */
-		if (notification->IsPaused() && myEndpoint && GetEnableHA()) {
-			Log(LogNotice, "NotificationComponent")
-				<< "Reminder notification '" << notificationName << "': HA cluster active, this endpoint does not have the authority (paused=true). Skipping.";
-			continue;
-		}
-
-		Checkable::Ptr checkable = notification->GetCheckable();
-
-		if (!IcingaApplication::GetInstance()->GetEnableNotifications() || !checkable->GetEnableNotifications())
-			continue;
-
-		if (notification->GetInterval() <= 0 && notification->GetNoMoreNotifications()) {
-			Log(LogNotice, "NotificationComponent")
-				<< "Reminder notification '" << notificationName << "': Notification was sent out once and interval=0 disables reminder notifications.";
-			continue;
-		}
-
-		if (notification->GetNextNotification() > now)
-			continue;
-
-		bool reachable = checkable->IsReachable(DependencyNotification);
-
-		{
-			ObjectLock olock(notification);
-			notification->SetNextNotification(Utility::GetTime() + notification->GetInterval());
-		}
-
-		{
-			Host::Ptr host;
-			Service::Ptr service;
-			tie(host, service) = GetHostService(checkable);
-
-			ObjectLock olock(checkable);
-
-			if (checkable->GetStateType() == StateTypeSoft)
-				continue;
-
-			/* Don't send reminder notifications for OK/Up states. */
-			if ((service && service->GetState() == ServiceOK) || (!service && host->GetState() == HostUp))
-				continue;
-
-			/* Skip in runtime filters. */
-			if (!reachable || checkable->IsInDowntime() || checkable->IsAcknowledged() || checkable->IsFlapping())
-				continue;
-		}
-
-		try {
-			Log(LogNotice, "NotificationComponent")
-				<< "Attempting to send reminder notification '" << notificationName << "'.";
-
-			notification->BeginExecuteNotification(NotificationProblem, checkable->GetLastCheckResult(), false, true);
-		} catch (const std::exception& ex) {
-			Log(LogWarning, "NotificationComponent")
-				<< "Exception occurred during notification for object '"
-				<< notificationName << "': " << DiagnosticInformation(ex, false);
-		}
+	if (type != StateTypeHard) {
+		Log(LogCritical, "DEBUG")
+				<< "Ignoring soft state change for " << checkable->GetName();
+		return;
 	}
+
+	for (const Notification::Ptr notifcation : checkable->GetNotifications()) {
+		Log(LogCritical, "DEBUG")
+			<< "Checkable " << checkable->GetName() << " had a hard change and wants to check Notification "
+			<< notifcation->GetName();
+			// Check if notification needs to be sent out
+
+		// Queue Renotifications
+	}
+
 }
 
-/**
- * Processes icinga::SendNotifications messages.
- */
-void NotificationComponent::SendNotificationsHandler(const Checkable::Ptr& checkable, NotificationType type,
-	const CheckResult::Ptr& cr, const String& author, const String& text)
-{
-	checkable->SendNotifications(type, cr, author, text);
-}
--- lib/notification/notificationcomponent.hpp
+++ lib/notification/notificationcomponent.hpp
@@ -6,7 +6,6 @@
 #include "notification/notificationcomponent-ti.hpp"
 #include "icinga/service.hpp"
 #include "base/configobject.hpp"
-#include "base/timer.hpp"
 
 namespace icinga
 {
@@ -20,17 +19,13 @@ public:
 	DECLARE_OBJECT(NotificationComponent);
 	DECLARE_OBJECTNAME(NotificationComponent);
 
-	static void StatsFunc(const Dictionary::Ptr& status, const Array::Ptr& perfdata);
-
 	void Start(bool runtimeCreated) override;
 	void Stop(bool runtimeRemoved) override;
 
-private:
-	Timer::Ptr m_NotificationTimer;
+	void StateChangeHandler(const Checkable::Ptr& checkable, const CheckResult::Ptr& cr, StateType type);
 
-	void NotificationTimerHandler();
-	void SendNotificationsHandler(const Checkable::Ptr& checkable, NotificationType type,
-		const CheckResult::Ptr& cr, const String& author, const String& text);
+private:
+
 };
 
 }
