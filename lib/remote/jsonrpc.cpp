e606d14705fb8aa43217ef1441aab7767a08fa2e
Michael Friedrich <michael.friedrich@icinga.com>
2019-05-23 17:25:56 +0200

    Quality: Clean JsonRPC class and add function docs

--- lib/remote/jsonrpc.cpp
+++ lib/remote/jsonrpc.cpp
@@ -15,6 +15,11 @@
 using namespace icinga;
 
 #ifdef I2_DEBUG
+/**
+ * Determine whether the developer wants to see raw JSON messages.
+ *
+ * @return Internal.DebugJsonRpc boolean
+ */
 static bool GetDebugJsonRpcCached()
 {
 	static int debugJsonRpc = -1;
@@ -47,25 +52,6 @@ static bool GetDebugJsonRpcCached()
  *
  * @return The amount of bytes sent.
  */
-size_t JsonRpc::SendMessage(const Stream::Ptr& stream, const Dictionary::Ptr& message)
-{
-	String json = JsonEncode(message);
-
-#ifdef I2_DEBUG
-	if (GetDebugJsonRpcCached())
-		std::cerr << ConsoleColorTag(Console_ForegroundBlue) << ">> " << json << ConsoleColorTag(Console_Normal) << "\n";
-#endif /* I2_DEBUG */
-
-	return NetString::WriteStringToStream(stream, json);
-}
-
-/**
- * Sends a message to the connected peer and returns the bytes sent.
- *
- * @param message The message.
- *
- * @return The amount of bytes sent.
- */
 size_t JsonRpc::SendMessage(const std::shared_ptr<AsioTlsStream>& stream, const Dictionary::Ptr& message)
 {
 	String json = JsonEncode(message);
@@ -90,13 +76,15 @@ size_t JsonRpc::SendMessage(const std::shared_ptr<AsioTlsStream>& stream, const 
 	return JsonRpc::SendRawMessage(stream, JsonEncode(message), yc);
 }
 
-/**
- * Sends a message to the connected peer and returns the bytes sent.
- *
- * @param message The message.
- *
- * @return The amount of bytes sent.
- */
+ /**
+  * Sends a raw message to the connected peer.
+  *
+  * @param stream ASIO TLS Stream
+  * @param json message
+  * @param yc Yield context required for ASIO
+  *
+  * @return bytes sent
+  */
 size_t JsonRpc::SendRawMessage(const std::shared_ptr<AsioTlsStream>& stream, const String& json, boost::asio::yield_context yc)
 {
 #ifdef I2_DEBUG
@@ -107,23 +95,14 @@ size_t JsonRpc::SendRawMessage(const std::shared_ptr<AsioTlsStream>& stream, con
 	return NetString::WriteStringToStream(stream, json, yc);
 }
 
-StreamReadStatus JsonRpc::ReadMessage(const Stream::Ptr& stream, String *message, StreamReadContext& src, bool may_wait, ssize_t maxMessageLength)
-{
-	String jsonString;
-	StreamReadStatus srs = NetString::ReadStringFromStream(stream, &jsonString, src, may_wait, maxMessageLength);
-
-	if (srs != StatusNewItem)
-		return srs;
-
-	*message = jsonString;
-
-#ifdef I2_DEBUG
-	if (GetDebugJsonRpcCached())
-		std::cerr << ConsoleColorTag(Console_ForegroundBlue) << "<< " << jsonString << ConsoleColorTag(Console_Normal) << "\n";
-#endif /* I2_DEBUG */
-
-	return StatusNewItem;
-}
+/**
+ * Reads a message from the connected peer.
+ *
+ * @param stream ASIO TLS Stream
+ * @param maxMessageLength maximum size of bytes read.
+ *
+ * @return A JSON string
+ */
 
 String JsonRpc::ReadMessage(const std::shared_ptr<AsioTlsStream>& stream, ssize_t maxMessageLength)
 {
@@ -137,6 +116,15 @@ String JsonRpc::ReadMessage(const std::shared_ptr<AsioTlsStream>& stream, ssize_
 	return std::move(jsonString);
 }
 
+/**
+ * Reads a message from the connected peer.
+ *
+ * @param stream ASIO TLS Stream
+ * @param yc Yield Context for ASIO
+ * @param maxMessageLength maximum size of bytes read.
+ *
+ * @return A JSON string
+ */
 String JsonRpc::ReadMessage(const std::shared_ptr<AsioTlsStream>& stream, boost::asio::yield_context yc, ssize_t maxMessageLength)
 {
 	String jsonString = NetString::ReadStringFromStream(stream, yc, maxMessageLength);
@@ -149,6 +137,13 @@ String JsonRpc::ReadMessage(const std::shared_ptr<AsioTlsStream>& stream, boost:
 	return std::move(jsonString);
 }
 
+/**
+ * Decode message, enforce a Dictionary
+ *
+ * @param message JSON string
+ *
+ * @return Dictionary ptr
+ */
 Dictionary::Ptr JsonRpc::DecodeMessage(const String& message)
 {
 	Value value = JsonDecode(message);
