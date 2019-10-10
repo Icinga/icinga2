d051d71b75490ac54c149eb6d8fd9bb5d6d9fbab
Michael Insel <michael@insel.email>
2019-10-10 18:39:33 +0200

    ChocoInstall: Update script to use checksum verification

--- choco/chocolateyInstall.ps1.cmake
+++ choco/chocolateyInstall.ps1.cmake
@@ -1,8 +1,20 @@
-﻿$packageName = 'icinga2'
-$installerType = 'msi'
-$url32 = 'https://packages.icinga.com/windows/Icinga2-v${CHOCO_VERSION_SHORT}-x86.msi'
+﻿$packageName= 'icinga2'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://packages.icinga.com/windows/Icinga2-v${CHOCO_VERSION_SHORT}-x86.msi'
 $url64 = 'https://packages.icinga.com/windows/Icinga2-v${CHOCO_VERSION_SHORT}-x86_64.msi'
-$silentArgs = '/qn /norestart'
-$validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0)
+  softwareName  = 'Icinga 2*'
+  checksum      = '' # TODO: Add checksum
+  checksumType  = 'sha256'
+  checksum64    = '' # TODO: Add checksum
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
