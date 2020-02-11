11f15b393e70a89fd59260dad21cf5242c256e62
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-02-11 13:07:37 +0100

    Implement UserspaceThread::Mutex and UserspaceThread::RecursiveMutex

--- lib/base/userspace-thread.cpp
+++ lib/base/userspace-thread.cpp
@@ -2,6 +2,8 @@
 
 #include "base/userspace-thread.hpp"
 #include <boost/context/continuation.hpp>
+#include <thread>
+#include <utility>
 
 using namespace icinga;
 
@@ -15,5 +17,89 @@ void UserspaceThread::Yield_()
 		m_Parent = nullptr;
 		parent = parent.resume();
 		m_Parent = &parent;
+	}
+}
+
+void UserspaceThread::Mutex::lock()
+{
+	while (!try_lock()) {
+		UserspaceThread::Yield_();
+	}
+}
+
+UserspaceThread::RecursiveMutex::RecursiveMutex() : m_Depth(0)
+{
+	m_KernelspaceOwner.store(std::thread::id());
+	m_UserspaceOwner.store(UserspaceThread::None);
+}
+
+void UserspaceThread::RecursiveMutex::lock()
+{
+	auto ust (UserspaceThread::GetID());
+
+	if (ust == UserspaceThread::None) {
+		auto me (std::this_thread::get_id());
+
+		if (m_KernelspaceOwner.load() == me) {
+			++m_Depth;
+		} else {
+			m_Mutex.lock();
+			m_KernelspaceOwner.store(std::move(me));
+			m_Depth = 1;
+		}
+	} else {
+		if (m_UserspaceOwner.load() == ust) {
+			++m_Depth;
+		} else {
+			m_Mutex.lock();
+			m_UserspaceOwner.store(ust);
+			m_Depth = 1;
+		}
+	}
+}
+
+bool UserspaceThread::RecursiveMutex::try_lock()
+{
+	auto ust (UserspaceThread::GetID());
+
+	if (ust == UserspaceThread::None) {
+		auto me (std::this_thread::get_id());
+
+		if (m_KernelspaceOwner.load() == me) {
+			++m_Depth;
+		} else {
+			if (!m_Mutex.try_lock()) {
+				return false;
+			}
+
+			m_KernelspaceOwner.store(std::move(me));
+			m_Depth = 1;
+		}
+	} else {
+		if (m_UserspaceOwner.load() == ust) {
+			++m_Depth;
+		} else {
+			if (!m_Mutex.try_lock()) {
+				return false;
+			}
+
+			m_UserspaceOwner.store(ust);
+			m_Depth = 1;
+		}
+	}
+
+	return true;
+}
+
+void UserspaceThread::RecursiveMutex::unlock()
+{
+	if (!--m_Depth) {
+		if (UserspaceThread::GetID() == UserspaceThread::None) {
+			m_KernelspaceOwner.store(std::thread::id());
+		} else {
+			m_UserspaceOwner.store(UserspaceThread::None);
+		}
+
+		m_Mutex.unlock();
 	}
 }
--- lib/base/userspace-thread.hpp
+++ lib/base/userspace-thread.hpp
@@ -7,7 +7,10 @@
 #include "base/logger.hpp"
 #include "base/object.hpp"
 #include "base/shared-object.hpp"
+#include <atomic>
 #include <boost/context/continuation.hpp>
+#include <cstdint>
+#include <thread>
 #include <utility>
 
 namespace icinga
@@ -21,7 +24,20 @@ namespace icinga
 class UserspaceThread : public SharedObject
 {
 public:
+	typedef void* ID;
+
+	static constexpr ID None = nullptr;
+
+	class Mutex;
+	class RecursiveMutex;
+
 	DECLARE_PTR_TYPEDEFS(UserspaceThread);
+
+	static inline
+	ID GetID()
+	{
+		return m_Parent;
+	}
 
 	static void Yield_();
 
@@ -70,6 +86,63 @@ private:
 			return std::move(parent);
 		});
 	}
+};
+
+/**
+ * Like std::mutex, but UserspaceThread-aware.
+ *
+ * @ingroup base
+ */
+class UserspaceThread::Mutex
+{
+public:
+	inline Mutex() = default;
+
+	Mutex(const Mutex&) = delete;
+	Mutex(Mutex&&) = delete;
+	Mutex& operator=(const Mutex&) = delete;
+	Mutex& operator=(Mutex&&) = delete;
+
+	void lock();
+
+	inline bool try_lock()
+	{
+		return !m_Locked.test_and_set(std::memory_order_acquire);
+	}
+
+	inline void unlock()
+	{
+		m_Locked.clear(std::memory_order_release);
+	}
+
+private:
+	std::atomic_flag m_Locked = ATOMIC_FLAG_INIT;
+};
+
+/**
+ * Like std::recursive_mutex, but UserspaceThread-aware.
+ *
+ * @ingroup base
+ */
+class UserspaceThread::RecursiveMutex
+{
+public:
+	RecursiveMutex();
+
+	RecursiveMutex(const RecursiveMutex&) = delete;
+	RecursiveMutex(RecursiveMutex&&) = delete;
+	RecursiveMutex& operator=(const RecursiveMutex&) = delete;
+	RecursiveMutex& operator=(RecursiveMutex&&) = delete;
+
+	void lock();
+	bool try_lock();
+	void unlock();
+
+private:
+	std::atomic<std::thread::id> m_KernelspaceOwner;
+	std::atomic<UserspaceThread::ID> m_UserspaceOwner;
+	uint_fast32_t m_Depth;
+	UserspaceThread::Mutex m_Mutex;
 };
 
 }
