511c81ab90505530c0e3d56018a429604e5f3bcc
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-02-18 10:58:08 +0100

    Allow Log as out for ShowCodeLocation()

--- lib/base/debuginfo.cpp
+++ lib/base/debuginfo.cpp
@@ -1,8 +1,6 @@
 /* Icinga 2 | (c) 2012 Icinga GmbH | GPLv2+ */
 
 #include "base/debuginfo.hpp"
-#include "base/convert.hpp"
-#include <fstream>
 
 using namespace icinga;
 
@@ -32,66 +30,4 @@ DebugInfo icinga::DebugInfoRange(const DebugInfo& start, const DebugInfo& end)
 	result.LastLine = end.LastLine;
 	result.LastColumn = end.LastColumn;
 	return result;
-}
-
-#define EXTRA_LINES 2
-
-void icinga::ShowCodeLocation(std::ostream& out, const DebugInfo& di, bool verbose)
-{
-	if (di.Path.IsEmpty())
-		return;
-
-	out << "Location: " << di;
-
-	std::ifstream ifs;
-	ifs.open(di.Path.CStr(), std::ifstream::in);
-
-	int lineno = 0;
-	char line[1024];
-
-	while (ifs.good() && lineno <= di.LastLine + EXTRA_LINES) {
-		if (lineno == 0)
-			out << "\n";
-
-		lineno++;
-
-		ifs.getline(line, sizeof(line));
-
-		for (int i = 0; line[i]; i++)
-			if (line[i] == '\t')
-				line[i] = ' ';
-
-		int extra_lines = verbose ? EXTRA_LINES : 0;
-
-		if (lineno < di.FirstLine - extra_lines || lineno > di.LastLine + extra_lines)
-			continue;
-
-		String pathInfo = di.Path + "(" + Convert::ToString(lineno) + "): ";
-		out << pathInfo;
-		out << line << "\n";
-
-		if (lineno >= di.FirstLine && lineno <= di.LastLine) {
-			int start, end;
-
-			start = 0;
-			end = strlen(line);
-
-			if (lineno == di.FirstLine)
-				start = di.FirstColumn - 1;
-
-			if (lineno == di.LastLine)
-				end = di.LastColumn;
-
-			if (start < 0) {
-				end -= start;
-				start = 0;
-			}
-
-			out << String(pathInfo.GetLength(), ' ');
-			out << String(start, ' ');
-			out << String(end - start, '^');
-
-			out << "\n";
-		}
-	}
 }
