b7bd1004ab5eff27a9b2a24e229d0335300fbc80
Maximilian Philipps <mphilipps@saltation.de>
2020-02-18 16:13:06 +0100

    Quote all variable uses.
    While $pid is unlikely to include a whitespace, that path to the logfile
    or the icinga2 binary may very well do. In either case, quoting them
    doesn't hurt.

--- etc/initsystem/icinga2.init.d.cmake
+++ etc/initsystem/icinga2.init.d.cmake
@@ -23,32 +23,32 @@ elif [ -f /etc/init.d/functions ]; then
 fi
 
 # load system specific defines
-SYSCONFIGFILE=@ICINGA2_SYSCONFIGFILE@
-if [ -f $SYSCONFIGFILE ]; then
-	. $SYSCONFIGFILE
+SYSCONFIGFILE="@ICINGA2_SYSCONFIGFILE@"
+if [ -f "$SYSCONFIGFILE" ]; then
+	. "$SYSCONFIGFILE"
 else
 	echo "Couldn't load system specific defines from $SYSCONFIGFILE. Using defaults."
 fi
 
 # Set defaults, to overwrite see "@ICINGA2_SYSCONFIGFILE@"
 
-: ${ICINGA2_USER:="@ICINGA2_USER@"}
-: ${ICINGA2_GROUP:="@ICINGA2_GROUP@"}
-: ${ICINGA2_COMMAND_GROUP:="@ICINGA2_COMMAND_GROUP@"}
-: ${DAEMON:="@CMAKE_INSTALL_FULL_SBINDIR@/icinga2"}
-: ${ICINGA2_CONFIG_FILE:="@ICINGA2_CONFIGDIR@/icinga2.conf"}
-: ${ICINGA2_ERROR_LOG:=@ICINGA2_LOGDIR@/error.log}
-: ${ICINGA2_STARTUP_LOG:=@ICINGA2_LOGDIR@/startup.log}
-: ${ICINGA2_PID_FILE:="@ICINGA2_INITRUNDIR@/icinga2.pid"}
+: "${ICINGA2_USER:="@ICINGA2_USER@"}"
+: "${ICINGA2_GROUP:="@ICINGA2_GROUP@"}"
+: "${ICINGA2_COMMAND_GROUP:="@ICINGA2_COMMAND_GROUP@"}"
+: "${DAEMON:="@CMAKE_INSTALL_FULL_SBINDIR@/icinga2"}"
+: "${ICINGA2_CONFIG_FILE:="@ICINGA2_CONFIGDIR@/icinga2.conf"}"
+: "${ICINGA2_ERROR_LOG:=@ICINGA2_LOGDIR@/error.log}"
+: "${ICINGA2_STARTUP_LOG:=@ICINGA2_LOGDIR@/startup.log}"
+: "${ICINGA2_PID_FILE:="@ICINGA2_INITRUNDIR@/icinga2.pid"}"
 
 # Load extra environment variables
 if [ -f /etc/default/icinga2 ]; then
 	. /etc/default/icinga2
 fi
 
-test -x $DAEMON || exit 5
+test -x "$DAEMON" || exit 5
 
-if [ ! -e $ICINGA2_CONFIG_FILE ]; then
+if [ ! -e "$ICINGA2_CONFIG_FILE" ]; then
 	echo "Config file '$ICINGA2_CONFIG_FILE' does not exist."
 	exit 6
 fi
@@ -60,9 +60,9 @@ getent group "$ICINGA2_COMMAND_GROUP" >/dev/null 2>&1 || { echo "Icinga command 
 # Start Icinga 2
 start() {
 	printf "Starting Icinga 2: "
-	@CMAKE_INSTALL_PREFIX@/lib/icinga2/prepare-dirs "$SYSCONFIGFILE"
+	"@CMAKE_INSTALL_PREFIX@/lib/icinga2/prepare-dirs" "$SYSCONFIGFILE"
 
-	if ! $DAEMON daemon -c $ICINGA2_CONFIG_FILE -d -e $ICINGA2_ERROR_LOG > $ICINGA2_STARTUP_LOG 2>&1; then
+	if ! "$DAEMON" daemon -c "$ICINGA2_CONFIG_FILE" -d -e "$ICINGA2_ERROR_LOG" > "$ICINGA2_STARTUP_LOG" 2>&1; then
 		echo "Error starting Icinga. Check '$ICINGA2_STARTUP_LOG' for details."
 		exit 1
 	else
@@ -74,7 +74,7 @@ start() {
 stop() {
 	printf "Stopping Icinga 2: "
 
-	if [ ! -e $ICINGA2_PID_FILE ]; then
+	if [ ! -e "$ICINGA2_PID_FILE" ]; then
 		echo "The PID file '$ICINGA2_PID_FILE' does not exist."
 		if [ "x$1" = "xnofail" ]; then
 			return
@@ -112,13 +112,13 @@ reload() {
 checkconfig() {
 	printf "Checking configuration: "
 
-	if ! $DAEMON daemon -c $ICINGA2_CONFIG_FILE -C > $ICINGA2_STARTUP_LOG 2>&1; then
+	if ! "$DAEMON" daemon -c "$ICINGA2_CONFIG_FILE" -C > "$ICINGA2_STARTUP_LOG" 2>&1; then
 		if [ "x$1" = "x" ]; then
-			cat $ICINGA2_STARTUP_LOG
+			cat "$ICINGA2_STARTUP_LOG"
 			echo "Icinga 2 detected configuration errors. Check '$ICINGA2_STARTUP_LOG' for details."
 			exit 1
 		else
-			echo "Not "$1"ing Icinga 2 due to configuration errors. Check '$ICINGA2_STARTUP_LOG' for details."
+			echo "Not \"$1\"ing Icinga 2 due to configuration errors. Check '$ICINGA2_STARTUP_LOG' for details."
 			if [ "x$2" = "xfail" ]; then
 				exit 1
 			fi
@@ -128,7 +128,7 @@ checkconfig() {
 	echo "Done"
 	# no arguments requires full output
 	if [ "x$1" = "x" ]; then
-		cat $ICINGA2_STARTUP_LOG
+		cat "$ICINGA2_STARTUP_LOG"
 	fi
 }
 
@@ -136,7 +136,7 @@ checkconfig() {
 status() {
 	printf "Icinga 2 status: "
 
-	if [ ! -e $ICINGA2_PID_FILE ]; then
+	if [ ! -e "$ICINGA2_PID_FILE" ]; then
 		echo "Not running"
 		exit 3
 	fi
