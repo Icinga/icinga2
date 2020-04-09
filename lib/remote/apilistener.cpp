0c5e89b8f30e504fdcf7c973d766900d066162d2
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-04-09 14:27:28 +0200

    ApiListener#PersistMessage(): persist messages to SQLite

--- lib/remote/apilistener.cpp
+++ lib/remote/apilistener.cpp
@@ -1198,28 +1198,49 @@ void ApiListener::PersistMessage(const Dictionary::Ptr& message, const ConfigObj
 
 	ASSERT(ts != 0);
 
-	Dictionary::Ptr pmessage = new Dictionary();
-	pmessage->Set("timestamp", ts);
+	auto localZone (Zone::GetLocalZone());
+	std::set<Zone::Ptr> targetZones ({localZone});
 
-	pmessage->Set("message", JsonEncode(message));
+	{
+		auto parent (localZone->GetParent());
 
-	if (secobj) {
-		Dictionary::Ptr secname = new Dictionary();
-		secname->Set("type", secobj->GetReflectionType()->GetName());
-		secname->Set("name", secobj->GetName());
-		pmessage->Set("secobj", secname);
+		if (parent) {
+			targetZones.emplace(std::move(parent));
+		}
 	}
 
-	std::unique_lock<std::mutex> lock(m_LogLock);
-	if (m_LogFile) {
-		NetString::WriteStringToStream(m_LogFile, JsonEncode(pmessage));
-		m_LogMessageCount++;
-		SetLogMessageTimestamp(ts);
+	for (auto& zone : ConfigType::GetObjectsByType<Zone>()) {
+		/* immediate child zone */
+		if (zone->GetParent() == localZone) {
+			targetZones.emplace(zone);
+		}
+	}
 
-		if (m_LogMessageCount > 50000) {
-			CloseLogFile();
-			RotateLogFile();
-			OpenLogFile();
+	if (secobj) {
+		auto current (targetZones.begin());
+		auto end (targetZones.end());
+
+		while (current != end) {
+			if ((*current)->CanAccessObject(secobj)) {
+				++current;
+			} else {
+				targetZones.erase(current++);
+			}
+		}
+	}
+
+	std::set<Endpoint::Ptr> targetEndpoints;
+
+	for (auto& zone : targetZones) {
+		auto endpoints (zone->GetEndpoints());
+		targetEndpoints.insert(endpoints.begin(), endpoints.end());
+	}
+
+	targetEndpoints.erase(GetLocalEndpoint());
+
+	for (auto& endpoint : targetEndpoints) {
+		if (ts > endpoint->GetLocalLogPosition()) {
+			endpoint->GetReplayLog().Log(ts, JsonEncode(message));
 		}
 	}
 }
@@ -1475,9 +1496,6 @@ void ApiListener::RotateLogFile()
 	if (!Utility::PathExists(newpath)) {
 		try {
 			Utility::RenameFile(oldpath, newpath);
-
-			// We're rotating the current log file, so reset the log message counter as well.
-			m_LogMessageCount = 0;
 		} catch (const std::exception& ex) {
 			Log(LogCritical, "ApiListener")
 				<< "Cannot rotate replay log file from '" << oldpath << "' to '"
