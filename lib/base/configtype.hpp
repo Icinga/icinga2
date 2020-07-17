b2fc49569c63545ffa9d9b3e88306ded9c9997bc
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-07-17 14:05:21 +0200

    Make ConfigType#m_Mutex a std::shared_timed_mutex
    refs #8112

--- lib/base/configtype.hpp
+++ lib/base/configtype.hpp
@@ -7,7 +7,7 @@
 #include "base/object.hpp"
 #include "base/type.hpp"
 #include "base/dictionary.hpp"
-#include <mutex>
+#include <shared_mutex>
 #include <unordered_map>
 
 namespace icinga
@@ -52,7 +52,7 @@ private:
 	typedef std::unordered_map<String, intrusive_ptr<ConfigObject> > ObjectMap;
 	typedef std::vector<intrusive_ptr<ConfigObject> > ObjectVector;
 
-	mutable std::mutex m_Mutex;
+	mutable std::shared_timed_mutex m_Mutex;
 	ObjectMap m_ObjectMap;
 	ObjectVector m_ObjectVector;
 
