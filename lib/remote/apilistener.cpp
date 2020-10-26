a530dd1b88180b5772fa81ef27646bbaaa2c37dd
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-10-26 11:00:54 +0100

    ApiListener#NewClientHandlerInternal(): reject connections from already connected endpoints

--- lib/remote/apilistener.cpp
+++ lib/remote/apilistener.cpp
@@ -657,6 +657,13 @@ void ApiListener::NewClientHandlerInternal(boost::asio::yield_context yc, const 
 	if (ctype == ClientJsonRpc) {
 		Log(LogNotice, "ApiListener", "New JSON-RPC client");
 
+		if (endpoint && endpoint->GetConnected()) {
+			Log(LogNotice, "ApiListener")
+				<< "Ignoring JSON-RPC connection " << conninfo
+				<< ". We're already connected to Endpoint '" << endpoint->GetName() << "'.";
+			return;
+		}
+
 		JsonRpcConnection::Ptr aclient = new JsonRpcConnection(identity, verify_ok, client, role);
 
 		if (endpoint) {
