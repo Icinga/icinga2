0698bdb013f1171ea6fc5c9c130e2647edad49ca
Julian Brost <julian.brost@icinga.com>
2021-06-30 16:00:57 +0200

    Add tests for DST handling in TimePeriods and ScheduledDowntimes

--- test/icinga-legacytimeperiod.cpp
+++ test/icinga-legacytimeperiod.cpp
@@ -7,6 +7,8 @@
 #include <boost/date_time/posix_time/posix_time_duration.hpp>
 #include <boost/date_time/gregorian/conversion.hpp>
 #include <boost/date_time/date.hpp>
+#include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
 #include <BoostTestTargetConfig.h>
 
 using namespace icinga;
@@ -17,13 +19,13 @@ struct GlobalTimezoneFixture
 {
 	char *tz;
 
-	GlobalTimezoneFixture()
+	GlobalTimezoneFixture(const char *fixed_tz = "")
 	{
 		tz = getenv("TZ");
 #ifdef _WIN32
-		_putenv_s("TZ", "UTC");
+		_putenv_s("TZ", fixed_tz == "" ? "UTC" : fixed_tz);
 #else
-		setenv("TZ", "", 1);
+		setenv("TZ", fixed_tz, 1);
 #endif
 		tzset();
 	}
@@ -263,6 +265,175 @@ BOOST_AUTO_TEST_CASE(advanced)
 	AdvancedHelper("09:00:01-30:00", {{2014, 9, 24}, {9, 0, 1}}, {{2014, 9, 25}, {6, 0, 0}});
 	AdvancedHelper("09:00-30:00:02", {{2014, 9, 24}, {9, 0, 0}}, {{2014, 9, 25}, {6, 0, 2}});
 	AdvancedHelper("09:00:03-30:00:04", {{2014, 9, 24}, {9, 0, 3}}, {{2014, 9, 25}, {6, 0, 4}});
+}
+
+tm make_tm(std::string s) {
+	tm t = {};
+	int dst = -1;
+	size_t l = strlen("YYYY-MM-DD HH:MM:SS");
+	if (s.size() > l) {
+		std::string zone = s.substr(l);
+		if (zone == " CET") {
+			dst = 0;
+		} else if (zone == " CEST") {
+			dst = 1;
+		} else {
+			// tests should only use CET/CEST (for now)
+			BOOST_CHECK_MESSAGE(false, "invalid or unknown time time: " << zone);
+		}
+	}
+	strptime(s.c_str(), "%Y-%m-%d %H:%M:%S", &t);
+	t.tm_isdst = dst;
+	return t;
+}
+
+time_t make_time_t(std::string s) {
+	tm t = make_tm(s);
+	return mktime(&t);
+}
+
+struct Segment {
+	time_t begin, end;
+
+	Segment(time_t begin, time_t end) : begin(begin), end(end) {}
+	Segment(std::string begin, std::string end) : begin(make_time_t(begin)), end(make_time_t(end)) {}
+
+	bool operator==(const Segment &o) const {
+		return o.begin == begin && o.end == end;
+	}
+};
+
+std::string pretty_time(tm t) {
+	char buf[128];
+	size_t n = strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %Z", &t);
+	return std::string(buf, n);
+}
+
+std::string pretty_time(time_t t) {
+	return pretty_time(Utility::LocalTime(t));
+}
+
+std::ostream& operator<<(std::ostream& o, const Segment& s) {
+	return o << "(" << pretty_time(s.begin) << " (" << s.begin << ") .. " << pretty_time(s.end) << " (" << s.end << "))";
+}
+
+BOOST_AUTO_TEST_CASE(dst)
+{
+	// DST changes in Europe/Berlin:
+	// 2021-03-28: 01:59:59 CET  (UTC+1) -> 03:00:00 CEST (UTC+2)
+	// 2021-10-31: 02:59:59 CEST (UTC+2) -> 02:00:00 CET  (UTC+1)
+	GlobalTimezoneFixture tz("Europe/Berlin");
+
+	// self-test for helper functions
+	BOOST_CHECK_EQUAL(make_tm("2021-10-31 02:30:00").tm_isdst, -1);
+	BOOST_CHECK_EQUAL(make_tm("2021-10-31 02:30:00 CET").tm_isdst, 0);
+	BOOST_CHECK_EQUAL(make_tm("2021-10-31 02:30:00 CEST").tm_isdst, 1);
+	BOOST_CHECK_EQUAL(make_time_t("2021-10-31 02:30:00 CET"), 1635643800); // date -d '2021-10-31 02:30:00 CET' +%s
+	BOOST_CHECK_EQUAL(make_time_t("2021-10-31 02:30:00 CEST"), 1635640200); // date -d '2021-10-31 02:30:00 CEST' +%s
+
+	struct TestData {
+		const char *day;
+		const char *ranges;
+		std::vector<tm> before;
+		std::vector<tm> during;
+		boost::optional<Segment> expected;
+	};
+
+	std::vector<TestData> tests {
+		// 2021-03-28: 01:59:59 CET (UTC+1) -> 03:00:00 CEST (UTC+2)
+		{
+			// range before DST change
+			"2021-03-28", "00:30-01:30",
+			{make_tm("2021-03-28 00:00:00 CET")},
+			{make_tm("2021-03-28 01:00:00 CET")},
+			Segment("2021-03-28 00:30:00 CET", "2021-03-28 01:30:00 CET"),
+		}, /*{
+			"2021-03-28", "01:30-02:30",
+			{make_tm("2021-03-28 01:00:00 CET")},
+			{make_tm("2021-03-28 01:59:59 CET")},
+			boost::none, // TODO: don't know what should actually happen...
+		}, {
+			"2021-03-28", "02:30-03:30",
+			{make_tm("2021-03-28 01:00:00 CET"), make_tm("2021-03-28 01:59:59 CET")},
+			{make_tm("2021-03-28 03:00:00 CEST")},
+			boost::none, // TODO: don't know what should actually happen...
+		},*/ {
+			// range after DST change
+			"2021-03-28", "03:30-04:30",
+			{make_tm("2021-03-28 01:00:00 CET"), make_tm("2021-03-28 03:00:00 CEST")},
+			{make_tm("2021-03-28 04:00:00 CEST")},
+			Segment("2021-03-28 03:30:00 CEST", "2021-03-28 04:30:00 CEST"),
+		}, {
+			// range containing DST change
+			"2021-03-28", "01:30-03:30",
+			{make_tm("2021-03-28 01:00:00 CET")},
+			{make_tm("2021-03-28 01:45:00 CET"), make_tm("2021-03-28 03:15:00 CEST")},
+			Segment("2021-03-28 01:30:00 CET", "2021-03-28 03:30:00 CEST"),
+		},
+
+		// 2021-10-31: 02:59:59 CEST (UTC+2) -> 02:00:00 CET (UTC+1)
+		{
+			// range before DST change
+			"2021-10-31", "00:30-01:30",
+			{make_tm("2021-10-31 00:00:00 CEST")},
+			{make_tm("2021-10-31 01:00:00 CEST")},
+			Segment("2021-10-31 00:30:00 CEST", "2021-10-31 01:30:00 CEST"),
+		}, {
+			// range existing twice during DST change (first instance)
+			"2021-10-31", "02:15-02:45",
+			{make_tm("2021-10-31 02:00:00 CEST")},
+			{make_tm("2021-10-31 02:30:00 CEST")},
+			Segment("2021-10-31 02:15:00 CEST", "2021-10-31 02:45:00 CEST"),
+		}, {
+			// range existing twice during DST change (second instance)
+			"2021-10-31", "02:15-02:45",
+			{make_tm("2021-10-31 02:00:00 CET")},
+			{make_tm("2021-10-31 02:30:00 CET")},
+			Segment("2021-10-31 02:15:00 CET", "2021-10-31 02:45:00 CET"),
+		}, {
+			// range after DST change
+			"2021-10-31", "03:30-04:30",
+			{make_tm("2021-10-31 01:00:00 CEST"), make_tm("2021-10-31 03:00:00 CET")},
+			{make_tm("2021-10-31 04:00:00 CET")},
+			Segment("2021-10-31 03:30:00 CET", "2021-10-31 04:30:00 CET"),
+		}, {
+			// range containing DST change
+			"2021-10-31", "01:30-03:30",
+			{make_tm("2021-10-31 01:00:00 CEST")},
+			{make_tm("2021-10-31 01:45:00 CEST"), make_tm("2021-10-31 02:30:00 CEST"), make_tm("2021-10-31 02:30:00 CET"), make_tm("2021-10-31 03:15:00 CET")},
+			Segment("2021-10-31 01:30:00 CEST", "2021-10-31 03:30:00 CET"),
+		},
+		// TODO: strange cases (interval overlaps 2:00 or 3:00), I really don't know what should actually happen...
+		//{"2021-10-31", "01:30-02:30", {make_tm("2021-10-31 01:00:00 CEST")}, {make_tm("2021-10-31 02:00:00 CEST")}, boost::none},
+		//{"2021-10-31", "02:30-03:30", {make_tm("2021-10-31 02:00:00 CEST")}, {make_tm("2021-10-31 03:00:00 CEST")}, boost::none},
+		//{"2021-10-31", "01:30-02:30", {make_tm("2021-10-31 01:00:00 CET")}, {make_tm("2021-10-31 02:00:00 CET")}, boost::none},
+		//{"2021-10-31", "02:30-03:30", {make_tm("2021-10-31 02:00:00 CET")}, {make_tm("2021-10-31 03:00:00 CET")}, boost::none},
+	};
+
+	auto seg = [](const Dictionary::Ptr& segment) -> boost::optional<Segment> {
+		if (segment == nullptr) {
+			return boost::none;
+		}
+
+		BOOST_CHECK(segment->Contains("begin"));
+		BOOST_CHECK(segment->Contains("end"));
+
+		return Segment{time_t(segment->Get("begin")), time_t(segment->Get("end"))};
+	};
+
+	for (const auto &t : tests) {
+		for (const tm& ref : t.during) {
+			tm mutRef = ref;
+			auto runningSeg = seg(LegacyTimePeriod::FindRunningSegment(t.day, t.ranges, &mutRef));
+			BOOST_CHECK_MESSAGE(runningSeg == t.expected, "FindRunningSegment(day='" << t.day << "' ranges='" << t.ranges << "' ref='" << pretty_time(ref) << "'): got=" << runningSeg << " expected=" << t.expected);
+		}
+
+		for (const tm& ref : t.before) {
+			tm mutRef = ref;
+			auto nextSeg = seg(LegacyTimePeriod::FindNextSegment(t.day, t.ranges, &mutRef));
+			BOOST_CHECK_MESSAGE(nextSeg == t.expected, "FindNextSegment(day='" << t.day << "' ranges='" << t.ranges << "' ref='" << pretty_time(ref) << "'): got=" << nextSeg << " expected=" << t.expected);
+		}
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
