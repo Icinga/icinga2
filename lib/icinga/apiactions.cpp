da394b2ab03f5cb47297eeca3967313953127322
Michael Insel <michael@insel.email>
2021-07-20 11:10:26 +0200

    Implement scheduling_source attribute (#6326)
    * Implement scheduling_source attribute
    
    This implements the attribute `scheduling_source` for hosts and services to show which endpoint is running the scheduler for the check.
    
    refs #4814

--- lib/icinga/apiactions.cpp
+++ lib/icinga/apiactions.cpp
@@ -104,6 +104,7 @@ Dictionary::Ptr ApiActions::ProcessCheckResult(const ConfigObject::Ptr& object,
 		cr->SetExecutionEnd(HttpUtility::GetLastParameter(params, "execution_end"));
 
 	cr->SetCheckSource(HttpUtility::GetLastParameter(params, "check_source"));
+	cr->SetSchedulingSource(HttpUtility::GetLastParameter(params, "scheduling_source"));
 
 	Value perfData = params->Get("performance_data");
 
