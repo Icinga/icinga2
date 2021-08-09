70b4558a62234aefa30d9fccdd98b08a6c28f52f
Alexander A. Klimov <alexander.klimov@icinga.com>
2021-08-09 20:07:38 +0200

    Icinga DB: introduce icinga:history:stream:downtime#scheduled_by
    ... with the Downtime#scheduled_by attribute.

--- lib/icingadb/icingadb-objects.cpp
+++ lib/icingadb/icingadb-objects.cpp
@@ -1761,6 +1761,13 @@ void IcingaDB::SendStartedDowntime(const Downtime::Ptr& downtime)
 		xAdd.emplace_back(GetObjectIdentifier(parent));
 	}
 
+	auto scheduledBy (downtime->GetScheduledBy());
+
+	if (!scheduledBy.IsEmpty()) {
+		xAdd.emplace_back("scheduled_by");
+		xAdd.emplace_back(scheduledBy);
+	}
+
 	m_Rcon->FireAndForgetQuery(std::move(xAdd), Prio::History);
 }
 
@@ -1839,6 +1846,13 @@ void IcingaDB::SendRemovedDowntime(const Downtime::Ptr& downtime)
 	if (parent) {
 		xAdd.emplace_back("parent_id");
 		xAdd.emplace_back(GetObjectIdentifier(parent));
+	}
+
+	auto scheduledBy (downtime->GetScheduledBy());
+
+	if (!scheduledBy.IsEmpty()) {
+		xAdd.emplace_back("scheduled_by");
+		xAdd.emplace_back(scheduledBy);
 	}
 
 	m_Rcon->FireAndForgetQuery(std::move(xAdd), Prio::History);
