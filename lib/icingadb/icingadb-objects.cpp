df84a498f4f4f99008ae4c1cf0588b3765b1c1c2
Julian Brost <julian.brost@icinga.com>
2021-10-08 11:15:47 +0200

    IcingaDB: actually write parent to parent_id of zones
    This fixes that the code used the wrong variable. Previously, it was written to
    Redis that each zone is its own parent (if it has a parent at all).

--- lib/icingadb/icingadb-objects.cpp
+++ lib/icingadb/icingadb-objects.cpp
@@ -1187,7 +1187,7 @@ bool IcingaDB::PrepareObject(const ConfigObject::Ptr& object, Dictionary::Ptr& a
 
 		Zone::Ptr parent = zone->GetParent();
 		if (parent) {
-			attributes->Set("parent_id", GetObjectIdentifier(zone));
+			attributes->Set("parent_id", GetObjectIdentifier(parent));
 		}
 
 		auto parentsRaw (zone->GetAllParentsRaw());
