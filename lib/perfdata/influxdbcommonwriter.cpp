99c5c24a17c7688c9e61c041d5d594150fe51763
Alexander Aleksandrovič Klimov <alexander.klimov@icinga.com>
2021-10-14 12:03:45 +0200

    InfluxdbCommonWriter#Flush(): fix log message
    s/InfluxdbWriter/Influxdb2Writer/
    
    fixes #9035

--- lib/perfdata/influxdbcommonwriter.cpp
+++ lib/perfdata/influxdbcommonwriter.cpp
@@ -460,7 +460,7 @@ void InfluxdbCommonWriter::Flush()
 	try {
 		stream = Connect();
 	} catch (const std::exception& ex) {
-		Log(LogWarning, "InfluxDbWriter")
+		Log(LogWarning, GetReflectionType()->GetName())
 			<< "Flush failed, cannot connect to InfluxDB: " << DiagnosticInformation(ex, false);
 		return;
 	}
