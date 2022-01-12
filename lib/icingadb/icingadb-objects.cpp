311429ceff7b2bf3a767ea8ed0b78a72c39c0388
Julian Brost <julian.brost@icinga.com>
2022-01-12 16:45:25 +0100

    Icinga DB: remove obsolete StateChangeHandler overload
    This version of StateChangeHandler is no longer called anywhere as it was the
    wrong function for all previous callers anyways.

--- lib/icingadb/icingadb-objects.cpp
+++ lib/icingadb/icingadb-objects.cpp
@@ -2591,15 +2591,6 @@ IcingaDB::UpdateObjectAttrs(const ConfigObject::Ptr& object, int fieldType,
 	//m_Rcon->FireAndForgetQuery({"HSET", keyPrefix + typeName, GetObjectIdentifier(object), JsonEncode(attrs)});
 }
 
-void IcingaDB::StateChangeHandler(const ConfigObject::Ptr& object)
-{
-	auto checkable (dynamic_pointer_cast<Checkable>(object));
-
-	if (checkable) {
-		IcingaDB::StateChangeHandler(object, checkable->GetLastCheckResult(), checkable->GetStateType());
-	}
-}
-
 void IcingaDB::StateChangeHandler(const ConfigObject::Ptr& object, const CheckResult::Ptr& cr, StateType type)
 {
 	for (const IcingaDB::Ptr& rw : ConfigType::GetObjectsByType<IcingaDB>()) {
