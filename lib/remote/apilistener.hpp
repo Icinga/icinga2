1492bffcccea7b527c61bf8606eaebf5a32eebe9
Alexander A. Klimov <alexander.klimov@icinga.com>
2022-03-29 16:36:13 +0200

    Introduce ApiListener#RenewCert()

--- lib/remote/apilistener.hpp
+++ lib/remote/apilistener.hpp
@@ -59,6 +59,7 @@ public:
 	static String GetCaDir();
 	static String GetCertificateRequestsDir();
 
+	std::shared_ptr<X509> RenewCert(const std::shared_ptr<X509>& cert);
 	void UpdateSSLContext();
 
 	static ApiListener::Ptr GetInstance();
