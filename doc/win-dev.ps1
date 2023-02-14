e812f3eeef5c381b81862f10f997817d6753b87b
Alexander A. Klimov <alexander.klimov@icinga.com>
2023-02-14 14:36:16 +0100

    Bump Boost to v1.81

--- doc/win-dev.ps1
+++ doc/win-dev.ps1
@@ -13,7 +13,7 @@ function ThrowOnNativeFailure {
 
 $VsVersion = 2019
 $MsvcVersion = '14.2'
-$BoostVersion = @(1, 80, 0)
+$BoostVersion = @(1, 81, 0)
 $OpensslVersion = '1_1_1t'
 
 switch ($Env:BITS) {
