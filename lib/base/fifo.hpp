78b4dc6509aa179a9b222e1a48e9b1939f59c1f1
Alexander A. Klimov <alexander.klimov@icinga.com>
2023-03-24 18:12:55 +0100

    Remove unused Stream#Peek()

--- lib/base/fifo.hpp
+++ lib/base/fifo.hpp
@@ -23,7 +23,6 @@ public:
 
 	~FIFO() override;
 
-	size_t Peek(void *buffer, size_t count, bool allow_partial = false) override;
 	size_t Read(void *buffer, size_t count, bool allow_partial = false) override;
 	void Write(const void *buffer, size_t count) override;
 	void Close() override;
