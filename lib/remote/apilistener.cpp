e74e1c8f8ae8444de40920983ebb853a69cf24ac
Alexander A. Klimov <alexander.klimov@icinga.com>
2023-08-22 12:32:38 +0200

    ApiListener#RelayMessage(): don't use m_RelayQueue

--- lib/remote/apilistener.cpp
+++ lib/remote/apilistener.cpp
@@ -1146,7 +1146,7 @@ void ApiListener::RelayMessage(const MessageOrigin::Ptr& origin,
 	if (!IsActive())
 		return;
 
-	m_RelayQueue.Enqueue([this, origin, secobj, message, log]() { SyncRelayMessage(origin, secobj, message, log); }, PriorityNormal, true);
+	SyncRelayMessage(origin, secobj, message, log);
 }
 
 void ApiListener::PersistMessage(const Dictionary::Ptr& message, const ConfigObject::Ptr& secobj)
