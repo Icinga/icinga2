7a21001d6d8d5e512688f78972074aeb226db59e
Alexander A. Klimov <alexander.klimov@icinga.com>
2023-08-28 15:52:04 +0200

    malloc(): call actual malloc(3)
    PoC: Icinga works again.

--- lib/base/allocator.cpp
+++ lib/base/allocator.cpp
@@ -3,14 +3,18 @@
 #ifndef _WIN32
 
 #include <cstddef>
+#include <dlfcn.h>
 
-extern "C" void* malloc(size_t)
+extern "C" void* malloc(size_t bytes)
 {
-	return nullptr;
+	static const auto libcMalloc = (void*(*)(size_t))dlsym(RTLD_NEXT, "malloc");
+	return libcMalloc(bytes);
 }
 
-extern "C" void free(void*)
+extern "C" void free(void* memory)
 {
+	static const auto libcFree = (void(*)(void*))dlsym(RTLD_NEXT, "free");
+	libcFree(memory);
 }
 
 #endif /* _WIN32 */
