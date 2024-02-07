9b3314f36cf830e4ac4fd1e93b283d8a8fff936f
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-02-07 11:24:10 +0100

    CpuBoundWork#~CpuBoundWork(): just call #Done() to deduplicate code

--- lib/base/io-engine.hpp
+++ lib/base/io-engine.hpp
@@ -42,7 +42,11 @@ public:
 	CpuBoundWork(CpuBoundWork&&) = delete;
 	CpuBoundWork& operator=(const CpuBoundWork&) = delete;
 	CpuBoundWork& operator=(CpuBoundWork&&) = delete;
-	~CpuBoundWork();
+
+	inline ~CpuBoundWork()
+	{
+		Done();
+	}
 
 	void Done();
 
