578275e836aab0dcc066cdbaa45a1d6073ab096b
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-11-06 13:30:28 +0100

    Consistently use Atomic, and not std::atomic
    Atomic enforces usage of its only safe constructor,
    in contrast to std::atomic.
    
    "The default-initialized std::atomic<T> does not contain
    a T object, and its only valid uses are destruction and
    initialization by std::atomic_init, see LWG issue 2334."
     -- https://en.cppreference.com/w/cpp/atomic/atomic/atomic

--- lib/base/atomic.hpp
+++ lib/base/atomic.hpp
@@ -74,7 +74,7 @@ private:
 };
 
 /**
- * Type alias for std::atomic<T> if possible, otherwise Locked<T> is used as a fallback.
+ * Type alias for Atomic<T> if possible, otherwise Locked<T> is used as a fallback.
  *
  * @ingroup base
  */
@@ -82,9 +82,9 @@ template <typename T>
 using AtomicOrLocked =
 #if defined(__GNUC__) && __GNUC__ < 5
 	// GCC does not implement std::is_trivially_copyable until version 5.
-	typename std::conditional<std::is_fundamental<T>::value || std::is_pointer<T>::value, std::atomic<T>, Locked<T>>::type;
+	typename std::conditional<std::is_fundamental<T>::value || std::is_pointer<T>::value, Atomic<T>, Locked<T>>::type;
 #else /* defined(__GNUC__) && __GNUC__ < 5 */
-	typename std::conditional<std::is_trivially_copyable<T>::value, std::atomic<T>, Locked<T>>::type;
+	typename std::conditional<std::is_trivially_copyable<T>::value, Atomic<T>, Locked<T>>::type;
 #endif /* defined(__GNUC__) && __GNUC__ < 5 */
 
 }
