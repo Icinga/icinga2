ecc087071959b83a42e30fe68e9694123d5a6457
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-11-08 11:36:49 +0100

    Remove unused EventQueue::UnregisterIfUnused()

--- lib/remote/eventqueue.cpp
+++ lib/remote/eventqueue.cpp
@@ -71,14 +71,6 @@ void EventQueue::RemoveClient(void *client)
 	m_Events.erase(client);
 }
 
-void EventQueue::UnregisterIfUnused(const String& name, const EventQueue::Ptr& queue)
-{
-	std::unique_lock<std::mutex> lock(queue->m_Mutex);
-
-	if (queue->m_Events.empty())
-		Unregister(name);
-}
-
 void EventQueue::SetTypes(const std::set<String>& types)
 {
 	std::unique_lock<std::mutex> lock(m_Mutex);
