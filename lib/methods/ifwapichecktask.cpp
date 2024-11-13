a6445c782b62414e9284a0727a6087b84e284029
Julian Brost <julian.brost@icinga.com>
2024-11-13 17:18:28 +0100

    Add comment for remaining uses of async_shutdown() why it's safe
    The reason for introducing AsioTlsStream::GracefulDisconnect() was to handle
    the TLS shutdown properly with a timeout since it involves a timeout. However,
    the implementation of this timeout involves spwaning coroutines which are
    redundant in some cases. This commit adds comments to the remaining calls of
    async_shutdown() stating why calling it is safe in these places.

--- lib/methods/ifwapichecktask.cpp
+++ lib/methods/ifwapichecktask.cpp
@@ -102,6 +102,8 @@ static void DoIfwNetIo(
 	}
 
 	{
+		// Using async_shutdown() instead of AsioTlsStream::GracefulDisconnect() as this whole function
+		// is already guarded by a timeout based on the check timeout.
 		boost::system::error_code ec;
 		sslConn.async_shutdown(yc[ec]);
 	}
