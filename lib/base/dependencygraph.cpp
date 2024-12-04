188ba53b74c530b323b9b45de34882d9444175af
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-12-04 10:57:30 +0100

    DependencyGraph: switch "parent" and "child" terminology
    The .ti files call `DependencyGraph::AddDependency(this, service.get())`. Obviously, `service.get()` is the parent and `this` (Downtime, Notification, ...) is the child. The DependencyGraph terminology should reflect this not to confuse its future users.

--- lib/base/dependencygraph.cpp
+++ lib/base/dependencygraph.cpp
@@ -7,18 +7,18 @@ using namespace icinga;
 std::mutex DependencyGraph::m_Mutex;
 std::map<Object *, std::map<Object *, int> > DependencyGraph::m_Dependencies;
 
-void DependencyGraph::AddDependency(Object *parent, Object *child)
+void DependencyGraph::AddDependency(Object* child, Object* parent)
 {
 	std::unique_lock<std::mutex> lock(m_Mutex);
-	m_Dependencies[child][parent]++;
+	m_Dependencies[parent][child]++;
 }
 
-void DependencyGraph::RemoveDependency(Object *parent, Object *child)
+void DependencyGraph::RemoveDependency(Object* child, Object* parent)
 {
 	std::unique_lock<std::mutex> lock(m_Mutex);
 
-	auto& refs = m_Dependencies[child];
-	auto it = refs.find(parent);
+	auto& refs = m_Dependencies[parent];
+	auto it = refs.find(child);
 
 	if (it == refs.end())
 		return;
@@ -29,15 +29,15 @@ void DependencyGraph::RemoveDependency(Object *parent, Object *child)
 		refs.erase(it);
 
 	if (refs.empty())
-		m_Dependencies.erase(child);
+		m_Dependencies.erase(parent);
 }
 
-std::vector<Object::Ptr> DependencyGraph::GetParents(const Object::Ptr& child)
+std::vector<Object::Ptr> DependencyGraph::GetChildren(const Object::Ptr& parent)
 {
 	std::vector<Object::Ptr> objects;
 
 	std::unique_lock<std::mutex> lock(m_Mutex);
-	auto it = m_Dependencies.find(child.get());
+	auto it = m_Dependencies.find(parent.get());
 
 	if (it != m_Dependencies.end()) {
 		typedef std::pair<Object *, int> kv_pair;
