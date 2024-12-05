2051d0d2198ba64f982d9b13c1ecbf9db34676b8
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-12-05 17:27:15 +0100

    CpuBoundWork#CpuBoundWork(): don't spin on atomic int to acquire slot
    This is inefficient and involves possible bad surprises regarding waiting durations on busy nodes. Instead, use AsioConditionVariable#Wait() if there are no free slots. It's notified by others' CpuBoundWork#~CpuBoundWork() once finished.

--- lib/base/io-engine.cpp
+++ lib/base/io-engine.cpp
@@ -16,30 +16,86 @@
 
 using namespace icinga;
 
-CpuBoundWork::CpuBoundWork(boost::asio::yield_context yc, boost::asio::io_context::strand&)
+CpuBoundWork::CpuBoundWork(boost::asio::yield_context yc, boost::asio::io_context::strand& strand)
 	: m_Done(false)
 {
-	auto& ioEngine (IoEngine::Get());
+	VERIFY(strand.running_in_this_thread());
 
-	for (;;) {
-		auto availableSlots (ioEngine.m_CpuBoundSemaphore.fetch_sub(1));
+	auto& ie (IoEngine::Get());
+	Shared<AsioConditionVariable>::Ptr cv;
 
-		if (availableSlots < 1) {
-			ioEngine.m_CpuBoundSemaphore.fetch_add(1);
-			IoEngine::YieldCurrentCoroutine(yc);
-			continue;
+	auto fastPath = [&ie](std::memory_order loadOrder = std::memory_order_relaxed) {
+		for (auto freeSlots (ie.m_CpuBoundSemaphore.load(loadOrder)); freeSlots > 0;) {
+			if (ie.m_CpuBoundSemaphore.compare_exchange_weak(
+				freeSlots,
+				freeSlots - 1,
+				std::memory_order_seq_cst, // The default memory order in case of success
+				loadOrder // A failure here is equivalent to loading the latest value
+			)) {
+				return true;
+			}
 		}
 
-		break;
+		return false;
+	};
+
+	while (!fastPath()) {
+		if (!cv) {
+			cv = Shared<AsioConditionVariable>::Make(ie.GetIoContext());
+
+			// The above line may take a little bit, so let's optimistically re-check
+			if (fastPath()) {
+				break;
+			}
+		}
+
+		{
+			std::unique_lock lock (ie.m_CpuBoundWaitingMutex);
+
+			// The above line may take even longer, so let's check again.
+			// Also mitigate lost wake-ups by synchronizing with Done() via an equivalently strong memory order.
+			// On details, see Done() implementation inline comments.
+			if (fastPath(std::memory_order_seq_cst)) {
+				break;
+			}
+
+			ie.m_CpuBoundWaiting.emplace_back(strand, cv);
+		}
+
+		cv->Wait(yc);
 	}
 }
 
 void CpuBoundWork::Done()
 {
 	if (!m_Done) {
-		IoEngine::Get().m_CpuBoundSemaphore.fetch_add(1);
-
 		m_Done = true;
+
+		auto& ie (IoEngine::Get());
+
+		// The constructor takes the slow path only if the semaphore is full,
+		// so we only have to wake up constructors if the semaphore was full.
+		// This works because after fetch_add(), fast paths in the constructor
+		// (at least if using std::memory_order_seq_cst!) will succeed.
+		if (ie.m_CpuBoundSemaphore.fetch_add(1, std::memory_order_seq_cst) < 1) {
+			// So now there are only slow path subscribers from just before the fetch_add() to be woken up.
+			// Precisely, only subscribers from just before the fetch_add() which turned 0 to 1.
+
+			decltype(ie.m_CpuBoundWaiting) subscribers;
+
+			{
+				// Due to the mutex, wake-ups obviously may happen only before or after constructor subscribtions.
+				// Ideally, this wake-up happens after any ongoing subscription (obviously).
+				std::unique_lock lock (ie.m_CpuBoundWaitingMutex);
+				std::swap(subscribers, ie.m_CpuBoundWaiting);
+			}
+			// But even if it happens before (and gets lost), it's not a problem because now the ongoing subscriber
+			// will lock the mutex and re-check the semaphore which is already >0 (fast path) due to fetch_add().
+
+			for (auto& subscriber : subscribers) {
+				boost::asio::post(subscriber.first, [cv = std::move(subscriber.second)] { cv->NotifyOne(); });
+			}
+		}
 	}
 }
 
