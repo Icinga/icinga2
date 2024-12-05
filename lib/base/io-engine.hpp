bb380d33e06c8d9c1c3dc6912f061d4e104b3471
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-12-05 11:08:37 +0100

    Introduce AsioConditionVariable

--- lib/base/io-engine.hpp
+++ lib/base/io-engine.hpp
@@ -146,6 +146,24 @@ private:
 };
 
 /**
+ * Condition variable which doesn't block I/O threads
+ *
+ * @ingroup base
+ */
+class AsioConditionVariable
+{
+public:
+	AsioConditionVariable(boost::asio::io_context& io);
+
+	void Wait(boost::asio::yield_context yc);
+	bool NotifyOne();
+	size_t NotifyAll();
+
+private:
+	boost::asio::deadline_timer m_Timer;
+};
+
+/**
  * I/O timeout emulator
  *
  * @ingroup base
