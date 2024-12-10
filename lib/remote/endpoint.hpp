32937bbeada38b9e3dc80e8316c4d3c04f85d4c1
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-12-10 15:59:10 +0100

    Benchmark message reading/waiting/processing time per endpoint

--- lib/remote/endpoint.hpp
+++ lib/remote/endpoint.hpp
@@ -5,6 +5,7 @@
 
 #include "remote/i2-remote.hpp"
 #include "remote/endpoint-ti.hpp"
+#include "base/atomic.hpp"
 #include "base/ringbuffer.hpp"
 #include <set>
 
@@ -43,6 +44,14 @@ public:
 	void AddMessageSent(int bytes);
 	void AddMessageReceived(int bytes);
 
+	void AddInputTimes(const AtomicDuration::Clock::duration& waitTime, const AtomicDuration::Clock::duration& readTime, const AtomicDuration::Clock::duration& semaphoreTime, const AtomicDuration::Clock::duration& processTime)
+	{
+		m_InputWaitTime += waitTime;
+		m_InputReadTime += readTime;
+		m_InputSemaphoreTime += semaphoreTime;
+		m_InputProcessTime += processTime;
+	}
+
 	double GetMessagesSentPerSecond() const override;
 	double GetMessagesReceivedPerSecond() const override;
 
@@ -61,6 +70,11 @@ private:
 	mutable RingBuffer m_MessagesReceived{60};
 	mutable RingBuffer m_BytesSent{60};
 	mutable RingBuffer m_BytesReceived{60};
+
+	AtomicDuration m_InputWaitTime;
+	AtomicDuration m_InputReadTime;
+	AtomicDuration m_InputSemaphoreTime;
+	AtomicDuration m_InputProcessTime;
 };
 
 }
