18814012cef811693c7af947ba8c98df7cfd17ed
Yonas Habteab <yonas.habteab@icinga.com>
2024-12-16 09:22:21 +0100

    Checkable: Use redundancy groups state in `IsReachable`

--- lib/icinga/checkable-dependency.cpp
+++ lib/icinga/checkable-dependency.cpp
@@ -73,11 +73,6 @@ bool Checkable::IsReachable(DependencyType dt, int rstack) const
 		return false;
 	}
 
-	for (const Checkable::Ptr& checkable : GetParents()) {
-		if (!checkable->IsReachable(dt, rstack + 1))
-			return false;
-	}
-
 	/* implicit dependency on host if this is a service */
 	const auto *service = dynamic_cast<const Service *>(this);
 	if (service && (dt == DependencyState || dt == DependencyNotification)) {
@@ -88,36 +83,15 @@ bool Checkable::IsReachable(DependencyType dt, int rstack) const
 		}
 	}
 
-	auto deps = GetDependencies();
-
-	std::unordered_map<std::string, Dependency::Ptr> violated; // key: redundancy group, value: nullptr if satisfied, violating dependency otherwise
-
-	for (const Dependency::Ptr& dep : deps) {
-		std::string redundancy_group = dep->GetRedundancyGroup();
-
-		if (!dep->IsAvailable(dt)) {
-			if (redundancy_group.empty()) {
+	for (auto& dependencyGroup : GetDependencyGroups()) {
+		if (!(dependencyGroup->GetState(dt, rstack + 1) & DependencyGroup::State::ReachableOK)) {
+			if (dependencyGroup->IsRedundancyGroup()) { // For non-redundant groups, this should already be logged.
 				Log(LogDebug, "Checkable")
-					<< "Non-redundant dependency '" << dep->GetName() << "' failed for checkable '" << GetName() << "': Marking as unreachable.";
-
-				return false;
+					<< "All dependencies in redundancy group '" << dependencyGroup->GetName() << "' have failed for checkable '"
+					<< GetName() << "': Marking as unreachable.";
 			}
-
-			// tentatively mark this dependency group as failed unless it is already marked;
-			//  so it either passed before (don't overwrite) or already failed (so don't care)
-			// note that std::unordered_map::insert() will not overwrite an existing entry
-			violated.insert(std::make_pair(redundancy_group, dep));
-		} else if (!redundancy_group.empty()) {
-			violated[redundancy_group] = nullptr;
+			return false;
 		}
-	}
-
-	auto violator = std::find_if(violated.begin(), violated.end(), [](auto& v) { return v.second != nullptr; });
-	if (violator != violated.end()) {
-		Log(LogDebug, "Checkable")
-			<< "All dependencies in redundancy group '" << violator->first << "' have failed for checkable '" << GetName() << "': Marking as unreachable.";
-
-		return false;
 	}
 
 	return true;
