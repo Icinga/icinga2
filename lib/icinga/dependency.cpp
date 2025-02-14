ad2f9955c2d35eb24536577e3164145ce38699f3
Yonas Habteab <yonas.habteab@icinga.com>
2025-02-14 16:53:47 +0100

    (Un)register dependencies from parent prior to child Checkable

--- lib/icinga/dependency.cpp
+++ lib/icinga/dependency.cpp
@@ -198,16 +198,22 @@ void Dependency::OnAllConfigLoaded()
 		AssertNoDependencyCycle(m_Parent, graph);
 	}
 
-	m_Child->AddDependency(this);
+	// Icinga DB will implicitly send config updates for the parent Checkable to refresh its affect{s,ed}_children
+	// columns when registering the dependency from the child Checkable. So, we need to register the dependency from
+	// the parent Checkable first, otherwise the config update of the parent Checkable change nothing at all.
 	m_Parent->AddReverseDependency(this);
+	m_Child->AddDependency(this);
 }
 
 void Dependency::Stop(bool runtimeRemoved)
 {
 	ObjectImpl<Dependency>::Stop(runtimeRemoved);
 
-	GetChild()->RemoveDependency(this, runtimeRemoved);
+	// Icinga DB will implicitly send config updates for the parent Checkable to refresh its affect{s,ed}_children
+	// columns when removing the dependency from the child Checkable. So, we need to remove the dependency from
+	// the parent Checkable first, otherwise the config update of the parent Checkable change nothing at all.
 	GetParent()->RemoveReverseDependency(this);
+	GetChild()->RemoveDependency(this, runtimeRemoved);
 }
 
 bool Dependency::IsAvailable(DependencyType dt) const
