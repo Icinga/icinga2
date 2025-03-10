500ad70b8cc89ce5c856bad91021ee76a6b84848
Julian Brost <julian.brost@icinga.com>
2025-03-10 11:56:23 +0100

    Implement std::hash<boost::intrusive_ptr<T>> for old Boost versions
    Boost only implements it iself starting from version 1.74, but a specialization
    of std::hash<> can be added trivially to allow the use of
    std::unordered_set<boost::intrusive_ptr<T>> and
    std::unordered_map<boost::intrusive_ptr<K>, V>.
    
    Being unable to use such types already came up a few types in the past, often
    resulting in the use of raw pointer instead which always involves an additional
    "is this safe?"/"could the object go out of scope?" discussion. This commit
    simply solves this for the future by simply allowing the use of intrusive_ptr
    in unordered containers.

---
+++ lib/base/intrusive-ptr.hpp
@@ -0,0 +1,22 @@
+/* Icinga 2 | (c) 2025 Icinga GmbH | GPLv2+ */
+
+#pragma once
+
+#include "base/i2-base.hpp"
+#include <memory>
+#include <boost/smart_ptr/intrusive_ptr.hpp>
+#include <boost/version.hpp>
+
+// std::hash is only implemented starting from Boost 1.74. Implement it ourselves for older version to allow using
+// boost::intrusive_ptr inside std::unordered_set<> or as the key of std::unordered_map<>.
+// https://github.com/boostorg/smart_ptr/commit/5a18ffdc5609a0e64b63e47cb81c4f0847e0c087
+#if BOOST_VERSION < 107400
+template<class T>
+struct std::hash<boost::intrusive_ptr<T>>
+{
+	std::size_t operator()(const boost::intrusive_ptr<T>& ptr) const noexcept
+	{
+		return std::hash<T*>{}(ptr.get());
+	}
+};
+#endif /* BOOST_VERSION < 107400 */
