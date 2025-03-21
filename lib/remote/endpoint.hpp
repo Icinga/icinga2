d63e728e51716130671399c27914d3e7e18329d5
Alexander A. Klimov <alexander.klimov@icinga.com>
2025-03-21 10:06:44 +0100

    Introduce Endpoint#seconds_{reading_messages,awaiting_semaphore,processing_messages} for the API

--- lib/remote/endpoint.hpp
+++ lib/remote/endpoint.hpp
@@ -57,6 +57,10 @@ public:
 	double GetBytesSentPerSecond() const override;
 	double GetBytesReceivedPerSecond() const override;
 
+	double GetSecondsReadingMessages() const override;
+	double GetSecondsAwaitingSemaphore() const override;
+	double GetSecondsProcessingMessages() const override;
+
 protected:
 	void OnAllConfigLoaded() override;
 
