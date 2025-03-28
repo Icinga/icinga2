079cd9b1dbb78055d14740d4e97af7ac726eae7c
Alexander A. Klimov <alexander.klimov@icinga.com>
2025-03-28 16:40:20 +0100

    Introduce CheckResultProducer interface

--- lib/icinga/checkresult.hpp
+++ lib/icinga/checkresult.hpp
@@ -10,6 +10,28 @@ namespace icinga
 {
 
 /**
+ * A component that produces check results and hence is responsible for them, e.g. CheckerComponent.
+ * I.e. on its shutdown it has to clean up and/or wait for its own ongoing checks if any.
+ *
+ * @ingroup icinga
+ */
+class CheckResultProducer
+{
+public:
+	/**
+	 * Requests to delay the producer shutdown (if any) for a CheckResult to be processed.
+	 *
+	 * @return Whether request was accepted.
+	 */
+	virtual bool try_lock_shared() noexcept = 0;
+
+	/**
+	 * Releases one semaphore slot acquired for CheckResult processing.
+	 */
+	virtual void unlock_shared() noexcept = 0;
+};
+
+/**
  * A check result.
  *
  * @ingroup icinga
