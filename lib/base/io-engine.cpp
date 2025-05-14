81a22ae66f7d19bb45a96239200454bf0c7e81d6
Alexander A. Klimov <alexander.klimov@icinga.com>
2025-05-14 15:33:43 +0200

    Remove unused IoEngine#m_AlreadyExpiredTimer

--- lib/base/io-engine.cpp
+++ lib/base/io-engine.cpp
@@ -141,9 +141,8 @@ boost::asio::io_context& IoEngine::GetIoContext()
 	return m_IoContext;
 }
 
-IoEngine::IoEngine() : m_IoContext(), m_KeepAlive(boost::asio::make_work_guard(m_IoContext)), m_Threads(decltype(m_Threads)::size_type(Configuration::Concurrency * 2u)), m_AlreadyExpiredTimer(m_IoContext)
+IoEngine::IoEngine() : m_IoContext(), m_KeepAlive(boost::asio::make_work_guard(m_IoContext)), m_Threads(decltype(m_Threads)::size_type(Configuration::Concurrency * 2u))
 {
-	m_AlreadyExpiredTimer.expires_at(boost::posix_time::neg_infin);
 	m_CpuBoundSemaphore.store(Configuration::Concurrency * 3u / 2u);
 
 	for (auto& thread : m_Threads) {
