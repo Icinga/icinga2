c42dc1e55aac5ef1c97b991bec7070de7684307c
Julian Brost <julian.brost@icinga.com>
2025-08-19 16:02:18 +0200

    CMake: remove logrotate version detection
    This was used to detect the presence of version 3.8.0 which introduced the "su"
    config option[^1]. It was releases in 2011, so I'd say it's time that we can
    just assume that it's supported.
    
    Additionally, this has a bit of a security impact as it defaults to not using
    the "su" option, which means that logrotate will do more than necessary as
    root. This happened with our packages as these were built without logrotate
    being installed, which caused the version detection to fail. Just assuming the
    new version here instead of adding it as a non-obvious build dependency should
    be the more robust fix.
    
    [^1]: https://github.com/logrotate/logrotate/blob/r3-8-0/CHANGES#L6-L8

--- etc/logrotate.d/icinga2.cmake
+++ etc/logrotate.d/icinga2.cmake
@@ -1,10 +1,11 @@
 @ICINGA2_LOGDIR@/icinga2.log @ICINGA2_LOGDIR@/debug.log {
 	daily
-	rotate 7@LOGROTATE_USE_SU@
+	rotate 7
+	su @ICINGA2_USER@ @ICINGA2_GROUP@
 	compress
 	delaycompress
 	missingok
-	notifempty@LOGROTATE_CREATE@
+	notifempty
 	postrotate
 		/bin/kill -USR1 $(cat @ICINGA2_INITRUNDIR@/icinga2.pid 2> /dev/null) 2> /dev/null || true
 	endscript
@@ -12,10 +13,11 @@
 
 @ICINGA2_LOGDIR@/error.log {
 	daily
-	rotate 90@LOGROTATE_USE_SU@
+	rotate 90
+	su @ICINGA2_USER@ @ICINGA2_GROUP@
 	compress
 	delaycompress
 	missingok
-	notifempty@LOGROTATE_CREATE@
+	notifempty
 	# TODO: figure out how to get Icinga to re-open this log file
 }
