050d0600e9b2a81cc8ca109476db5668ee74a256
Johannes Schmidt <johannes.schmidt@icinga.com>
2025-09-22 12:31:02 +0200

    Filter global variables when Sandboxed

--- lib/base/scriptframe.hpp
+++ lib/base/scriptframe.hpp
@@ -5,6 +5,7 @@
 
 #include "base/i2-base.hpp"
 #include "base/dictionary.hpp"
+#include "base/namespace.hpp"
 #include "base/scriptpermission.hpp"
 #include <boost/thread/tss.hpp>
 #include <stack>
@@ -12,6 +13,16 @@
 namespace icinga
 {
 
+/**
+ * A frame describing the context a section of script code is executed in.
+ *
+ * This is implemented by each new object that is constructed getting pushed on a thread_local
+ * global stack that is accessible from anywhere during script evaluation.
+ *
+ * Most properties in this frame, like local variables do not carry over to successive frames,
+ * except the `PermChecker` and `Sandboxed` members, which get propagated to enforce access
+ * control and availability of unsafe functions.
+ */
 struct ScriptFrame
 {
 	Dictionary::Ptr Locals;
@@ -29,7 +40,20 @@ struct ScriptFrame
 
 	static ScriptFrame *GetCurrentFrame();
 
+	Namespace::Ptr GetGlobals();
+
 private:
+	/**
+	 * Caches a sanitized version of the global namespace for the current `ScriptFrame`.
+	 *
+	 * This is a value that is dependent on a ScriptFrame's `Sandboxed` and `CheckPerms`
+	 * members. These are both independent of each other and while they are inherited by
+	 * subsequent frames themselves, their values can be changed for new frames easily.
+	 * Therefore Globals can hold a different value for each ScriptFrame and is not
+	 * inherited.
+	 */
+	Namespace::Ptr Globals;
+
 	static boost::thread_specific_ptr<std::stack<ScriptFrame *> > m_ScriptFrames;
 
 	static void PushFrame(ScriptFrame *frame);
