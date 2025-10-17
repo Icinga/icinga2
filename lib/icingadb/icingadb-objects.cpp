93e69ed0e868505946d02b220eea7a025205c480
Johannes Schmidt <johannes.schmidt@icinga.com>
2025-10-17 14:27:18 +0200

    Fix -Wsign-compare warnings

--- lib/icingadb/icingadb-objects.cpp
+++ lib/icingadb/icingadb-objects.cpp
@@ -549,7 +549,7 @@ std::vector<std::vector<intrusive_ptr<ConfigObject>>> IcingaDB::ChunkObjects(std
 
 	chunks.reserve((std::distance(offset, end) + chunkSize - 1) / chunkSize);
 
-	while (std::distance(offset, end) >= chunkSize) {
+	while (static_cast<std::size_t>(std::distance(offset, end)) >= chunkSize) {
 		auto until (offset + chunkSize);
 		chunks.emplace_back(offset, until);
 		offset = until;
