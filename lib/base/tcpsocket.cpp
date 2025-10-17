8f1f5b5afb785f855235dce131203b1e79b0dcfa
Johannes Schmidt <johannes.schmidt@icinga.com>
2025-10-17 14:16:14 +0200

    fix -Wmaybe-uninitialized warnings

--- lib/base/tcpsocket.cpp
+++ lib/base/tcpsocket.cpp
@@ -32,8 +32,8 @@ void TcpSocket::Bind(const String& node, const String& service, int family)
 {
 	addrinfo hints;
 	addrinfo *result;
-	int error;
-	const char *func;
+	int error{};
+	const char *func{};
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = family;
@@ -126,8 +126,8 @@ void TcpSocket::Connect(const String& node, const String& service)
 {
 	addrinfo hints;
 	addrinfo *result;
-	int error;
-	const char *func;
+	int error{};
+	const char *func{};
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
