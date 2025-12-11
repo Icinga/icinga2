5892cb729dc7ee671e0fd44ae84e832ba4f96fef
Johannes Schmidt <johannes.schmidt@icinga.com>
2025-12-11 15:16:50 +0100

    (WIP) Abstract out connection-handling for Perfdata-Writers

---
+++ test/perfdata-perfdatawriterfixture.hpp
@@ -0,0 +1,120 @@
+/* Icinga 2 | (c) 2025 Icinga GmbH | GPLv2+ */
+
+#pragma once
+
+#include <BoostTestTargetConfig.h>
+#include "base/perfdatavalue.hpp"
+#include "base/wait-group.hpp"
+#include "config/configcompiler.hpp"
+#include "config/configitem.hpp"
+#include "icinga/host.hpp"
+#include "test/base-testloggerfixture.hpp"
+#include "test/perfdata-perfdatawriterconnectionfixture.hpp"
+#include <boost/hana.hpp>
+#include <boost/random.hpp>
+
+namespace icinga {
+
+template<typename Writer>
+class PerfdataWriterFixture : public PerfdataWriterConnectionFixture, public TestLoggerFixture
+{
+public:
+	PerfdataWriterFixture() : m_Writer(new Writer)
+	{
+		auto createObjects = [&]() {
+			String config = R"CONFIG(
+object CheckCommand "dummy" {
+	command = "/bin/echo"
+}
+object Host "h1" {
+	address = "h1"
+	check_command = "dummy"
+	enable_notifications = true
+	enable_active_checks = false
+	enable_passive_checks = true
+}
+)CONFIG";
+
+			std::unique_ptr<Expression> expr = ConfigCompiler::CompileText("<test>", config);
+			expr->Evaluate(*ScriptFrame::GetCurrentFrame());
+		};
+
+		ConfigItem::RunWithActivationContext(new Function("CreateTestObjects", createObjects));
+
+		m_Host = Host::GetByName("h1");
+		BOOST_REQUIRE(m_Host);
+
+		m_Writer->SetPort(std::to_string(GetPort()));
+		m_Writer->SetName(m_Writer->GetReflectionType()->GetName());
+		m_Writer->SetDisconnectTimeout(0.05);
+
+		auto hasFlushInterval = boost::hana::is_valid([](auto&& obj) -> decltype(obj.SetFlushInterval(0.05)) {});
+		if constexpr (decltype(hasFlushInterval(std::declval<Writer>()))::value) {
+			m_Writer->SetFlushInterval(0.05);
+		}
+		static_cast<ConfigObject::Ptr>(m_Writer)->OnConfigLoaded();
+		m_Writer->SetActive(true);
+		m_Writer->Activate();
+		BOOST_REQUIRE(!m_Writer->IsPaused());
+	}
+
+	static std::string GetRandomString(std::string prefix, std::size_t length)
+	{
+		std::string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		boost::random::mt19937 generator;
+		boost::random::uniform_int_distribution<> distribution(0, alphabet.size() - 1);
+
+		for (auto i = 0U; i < length; i++) {
+			prefix += alphabet[distribution(generator)];
+		}
+
+		return prefix;
+	}
+
+	/**
+	 * Make our test host receive a number of check-results.
+	 *
+	 * @param num The number of check-results to receive
+	 * @param state The state the check results should have
+	 * @param fn A function that will be passed the current check-result
+	 */
+	void ReceiveCheckResults(
+		std::size_t num,
+		ServiceState state,
+		const std::function<void(const CheckResult::Ptr&)>& fn = {}
+	)
+	{
+		StoppableWaitGroup::Ptr wg = new StoppableWaitGroup();
+
+		for (auto i = 0UL; i < num; ++i) {
+			CheckResult::Ptr cr = new CheckResult();
+
+			cr->SetState(state);
+
+			double now = Utility::GetTime();
+			cr->SetActive(false);
+			cr->SetScheduleStart(now);
+			cr->SetScheduleEnd(now);
+			cr->SetExecutionStart(now);
+			cr->SetExecutionEnd(now);
+
+			Array::Ptr perfData = new Array;
+			perfData->Add(new PerfdataValue{"thing", 42});
+			cr->SetPerformanceData(perfData);
+
+			if (fn) {
+				fn(cr);
+			}
+
+			BOOST_REQUIRE(m_Host->ProcessCheckResult(cr, wg) == Checkable::ProcessingResult::Ok);
+		}
+	}
+
+	void Pause() { static_cast<ConfigObject::Ptr>(m_Writer)->Pause(); }
+
+private:
+	Host::Ptr m_Host;
+	typename Writer::Ptr m_Writer;
+};
+
+} // namespace icinga
