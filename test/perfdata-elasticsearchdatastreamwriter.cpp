d142990dfe27f88ea97c0ae83fc3cccc99c0f36e
Johannes Schmidt <johannes.schmidt@icinga.com>
2026-02-03 09:02:15 +0100

    Add test-case to test connecting and pausing ElasticsearchDatastreamWriter

--- test/perfdata-elasticsearchdatastreamwriter.cpp
+++ test/perfdata-elasticsearchdatastreamwriter.cpp
@@ -18,9 +18,11 @@ BOOST_FIXTURE_TEST_SUITE(perfdata_elasticsearchdatastreamwriter, PerfdataWriterF
 
 BOOST_AUTO_TEST_CASE(connect)
 {
-	Logger::SetConsoleLogSeverity(LogDebug);
-	Logger::EnableConsoleLog();
-	ReceiveCheckResults(1, ServiceState::ServiceCritical);
+	GetWriter()->SetManageIndexTemplate(false);
+	ResumeWriter();
+	
+	ReceiveCheckResults(2, ServiceState::ServiceCritical);
+	ReceiveCheckResults(1, ServiceState::ServiceOK);
 
 	Accept();
 	auto resp = GetSplitDecodedRequestBody();
@@ -30,14 +32,20 @@ BOOST_AUTO_TEST_CASE(connect)
 	// correct here.
 	BOOST_REQUIRE_GT(resp->GetLength(), 1);
 	Dictionary::Ptr cr = resp->Get(1);
+	BOOST_REQUIRE(cr);
 	BOOST_CHECK(cr->Contains("@timestamp"));
-	BOOST_CHECK_EQUAL(cr->Get("check_command"), "dummy");
-	BOOST_CHECK_EQUAL(cr->Get("host"), "h1");
-	Pause();
+	Dictionary::Ptr host = cr->Get("host");
+	BOOST_REQUIRE(host);
+	BOOST_CHECK_EQUAL(host->Get("hostname"), "h1");
+
+	PauseWriter();
 }
 
 BOOST_AUTO_TEST_CASE(pause_with_pending_work)
 {
+	GetWriter()->SetManageIndexTemplate(false);
+	ResumeWriter();
+
 	ReceiveCheckResults(1, ServiceState::ServiceCritical, [](const CheckResult::Ptr& cr) {
 		cr->SetOutput(GetRandomString("####", 1024UL * 1024));
 	});
@@ -47,10 +55,10 @@ BOOST_AUTO_TEST_CASE(pause_with_pending_work)
 	GetDataUntil("####");
 
 	// Now try to pause.
-	Pause();
+	PauseWriter();
 
 	REQUIRE_LOG_MESSAGE("Operation cancelled\\.", 10s);
-	REQUIRE_LOG_MESSAGE("'ElasticsearchWriter' paused\\.", 10s);
+	REQUIRE_LOG_MESSAGE("'ElasticsearchDatastreamWriter' paused\\.", 10s);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
