b0df597e36e4f626e9306582043529e13df8651d
lersveen <7195448+lersveen@users.noreply.github.com>
2026-03-05 16:04:38 +0100

    fix: pass chain to VerifyCertificate

--- lib/base/tlsutility.cpp
+++ lib/base/tlsutility.cpp
@@ -1059,10 +1059,10 @@ String BinaryToHex(const unsigned char* data, size_t length) {
 
 bool VerifyCertificate(const std::shared_ptr<X509> &caCertificate, const std::shared_ptr<X509> &certificate, const String& crlFile, STACK_OF(X509) *chain)
 {
-	return VerifyCertificate(caCertificate.get(), certificate.get(), crlFile);
+	return VerifyCertificate(caCertificate.get(), certificate.get(), crlFile, chain);
 }
 
-bool VerifyCertificate(X509* caCertificate, X509* certificate, const String& crlFile)
+bool VerifyCertificate(X509* caCertificate, X509* certificate, const String& crlFile, STACK_OF(X509) *chain)
 {
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 	/*
@@ -1094,7 +1094,7 @@ bool VerifyCertificate(X509* caCertificate, X509* certificate, const String& crl
 	}
 
 	std::unique_ptr<X509_STORE_CTX, decltype(&X509_STORE_CTX_free)> csc{X509_STORE_CTX_new(), &X509_STORE_CTX_free};
-	X509_STORE_CTX_init(csc.get(), store.get(), certificate, nullptr);
+	X509_STORE_CTX_init(csc.get(), store.get(), certificate, chain);
 
 	int rc = X509_verify_cert(csc.get());
 
