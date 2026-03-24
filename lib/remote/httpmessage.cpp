ed403294a345c55074ff1a64ea73da7fe4c2fea4
Julian Brost <julian.brost@icinga.com>
2026-03-24 17:52:33 +0100

    OutgoingHttpMessage: don't use shared_ptr for m_CpuBoundWork
    This change just gives clear ownership over the CpuBoundWork to the
    OutgoingHttpMessage, instead of the previous shared_ptr and weak_ptr
    combination with an unclear purpose.

--- lib/remote/httpmessage.cpp
+++ lib/remote/httpmessage.cpp
@@ -146,10 +146,7 @@ void OutgoingHttpMessage<isRequest, Body, StreamVariant>::Clear()
 template<bool isRequest, typename Body, typename StreamVariant>
 void OutgoingHttpMessage<isRequest, Body, StreamVariant>::Flush(boost::asio::yield_context yc, bool finish)
 {
-	if (auto work (m_CpuBoundWork.lock()); work) {
-		work->Done();
-		m_CpuBoundWork.reset();
-	}
+	m_CpuBoundWork.reset();
 
 	if (!Base::chunked() && !Base::has_content_length()) {
 		ASSERT(!m_SerializationStarted);
