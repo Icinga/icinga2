324d5bb815f95deb7b87b22e42c6f152cdfd352e
Julian Brost <julian.brost@icinga.com>
2026-05-22 17:06:27 +0200

    Add filter-expression permission
    This allows preventing ApiUsers from evaluating their own DSL expressions for
    improved security.

--- lib/remote/deleteobjecthandler.cpp
+++ lib/remote/deleteobjecthandler.cpp
@@ -58,6 +58,9 @@ bool DeleteObjectHandler::HandleRequest(
 
 	try {
 		objs = FilterUtility::GetFilterTargets(qd, params, user);
+	} catch (const MissingPermissionError& ex) {
+		HttpUtility::SendJsonError(response, params, 403, ex.what());
+		return true;
 	} catch (const std::exception& ex) {
 		HttpUtility::SendJsonError(response, params, 404,
 			"No objects found.",
