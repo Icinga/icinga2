cd305de40826418c7b2f3289a8d4d0bc69096a4b
Julian Brost <julian.brost@icinga.com>
2026-06-12 10:56:43 +0200

    Bump OpenSSL shipped for Windows to v3.0.21
    (cherry picked from commit 767da5f4cdf57d8c24000bda2fcea81a553b6bb1)

--- doc/win-dev.ps1
+++ doc/win-dev.ps1
@@ -14,7 +14,7 @@ function ThrowOnNativeFailure {
 $VsVersion = 2022
 $MsvcVersion = '14.3'
 $BoostVersion = @(1, 87, 0)
-$OpensslVersion = '3_0_20'
+$OpensslVersion = '3_0_21'
 
 switch ($Env:BITS) {
 	32 { }
