87a18305d26c27e04deaaf14ee658f9a8634bd3c
Johannes Schmidt <johannes.schmidt@icinga.com>
2026-07-28 11:41:09 +0200

    Fix saving restoring ccache in Linux GHAs
    Previously the cache was never updated, because the key based on matrix.distro
    always hits and thus the action never updates the cache.
    
    This means we were effectively always running with the same stale old cache
    entry that was generated a long time ago, when the action was introduced,
    because the only other reason for GitHub to invalidate the cache is if it
    remains unused for >7 days, effectively meaning never, because there is always
    *some* activity in this repo.
    
    This changes it so each run on the master saves a new cache entry, overwriting
    the previous one that GitHub will then prune once the combined sizes of the
    entries are above 10GB. The new entry will however still contain a reasonable
    amount of old cache entries.

--- .github/workflows/linux.bash
+++ .github/workflows/linux.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -exo pipefail
 
-export PATH="/usr/lib/ccache/bin:/usr/lib/ccache:/usr/lib64/ccache:$PATH"
 export CCACHE_DIR=/icinga2/ccache
+# Limit ccache so all targets together don't ever get over the 10 GiB limit
+# that would cause the cache of the first action to be evicted.
+export CCACHE_MAXSIZE=400M
 export CTEST_OUTPUT_ON_FAILURE=1
 CMAKE_OPTS=()
 SCL_ENABLE_GCC=()
@@ -127,6 +129,18 @@ case "$DISTRO" in
     ;;
 esac
 
+# ccache isn't packaged for every distro (currently only amazonlinux).
+# We add it at the CMake layer because that's more compatible with distros
+# that use their own compiler wrappers.
+if command -v ccache >/dev/null; then
+  CMAKE_OPTS+=(
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+  )
+  # Zero the stats so the summary below reflects only this run.
+  ccache -z
+fi
+
 mkdir /icinga2/build
 cd /icinga2/build
 
@@ -140,6 +154,17 @@ cd /icinga2/build
   "${CMAKE_OPTS[@]}" ..
 
 ninja -v
+
+# Print ccache stats
+if command -v ccache >/dev/null; then
+  ccache -s
+  # Not all distros have a ccache version that prints precise cache size values.
+  # Some have ccache --print-stats but others don't. This just prints the size
+  # of the cache directory in MiB.
+  ccache_kib=$(du -sk "$CCACHE_DIR" | cut -f1)
+  printf 'exact ccache size (du): %s KiB (%d.%02d MiB)\n' \
+    "$ccache_kib" "$((ccache_kib / 1024))" "$((ccache_kib * 100 / 1024 % 100))"
+fi
 
 ninja test
 ninja install
